77ed572687913cfb902d32f2c4576f67f881e9d6
Olivier Boudeville <olivier.boudeville@esperide.com>
2007-01-07 17:02:26 +0000

    Prefixes and options managed for different uses.

--- src/conf/build/autogen.sh
+++ src/conf/build/autogen.sh
@@ -1,30 +1,40 @@
-#!/bin/bash
+#!/bin/sh
 
-USAGE="Usage : "`basename $0`" [ -h | --help ] [ -n | --no-build ] [ -c | --chain-test ] [ -f | --full-test ] [ -o | --only-prepare-dist ] [ --loani-repository <path> ] [ --loani-installations <path> ] [ --ceylan-install-prefix <path> ] [ --configure-options [option 1] [option 2] [...] ] : (re)generates all the autotools-based build system.\n\t --disable-all-features : build a library with none of the optional features\n\t --no-build : stop just after having generated the configure script\n\t --chain-test : build and install the library, build the test suite and run it against the installation\n\t --full-test : build and install the library, perform all available tests, including 'make distcheck' and the full test suite\n\t --only-prepare-dist : configure all but do not build anything\n\t --loani-repository : specify where the LOANI repository can be found. If none is specified, it will be guessed from the current directory\n\t --loani-installations : specify where the LOANI installations can be found. If none is specified, it will be guessed from the current directory\n\t --ceylan-install-prefix : specify where the Ceylan installation can be found.\n\t --configure-options : all following options will be directly passed whenever configure is run"
+USAGE="
+Usage : "`basename $0`" --with-osdl-environment <path> || --ceylan-install-prefix <path> [ -h | --help ] [ -n | --no-build ] [ -c | --chain-test ] [ -f | --full-test ] [ -o | --only-prepare-dist ] [ --configure-options [option 1] [option 2] [...] ] : (re)generates all the autotools-based build system.
 
+	--with-osdl-environment : specify where the OSDL environment file (OSDL-environment.sh) should be read
+	--ceylan-install-prefix : specify where the Ceylan installation can be found
+	Note : if neither --with-osdl-environment nor --ceylan-install-prefix are specified, the location of OSDL environment file will .	
+	--no-build : stop just after having generated the configure script
+	--chain-test : build and install the library, build the test suite and run it against the installation
+	--full-test : build and install the library, perform all available tests, including 'make distcheck' and the full test suite
+	--only-prepare-dist : configure all but do not build anything
+	--configure-options : all following options will be directly passed whenever configure is run (they will override default ones)"
+
+# This script must be used :
+#   - by loani-requiredTools.sh, only to produce the configure script (only
+# Ceylan installation location is needed, so that substitute.sh is found)
+#   - by the OSDL developer, who does not have to specify all tool prefixes,
+# thanks to OSDL-environment.sh
+# Hence this script does not have to recognize specific options to manage
+# the prefix of each specific tool.
 
 # Main settings section.
 
 osdl_features_disable_opt=""
+configure_user_opt=""
 
 osdl_features_opt=""
+
+# At least one of the two will have to be set :
+osdl_environment_file=""
+ceylan_location=""
 
 
 # To check the user can override them :
 #test_overriden_options="CPPFLAGS=\"-DTEST_CPPFLAGS\" LDFLAGS=\"-LTEST_LDFLAGS\""
 
-# Default value guessed from current path :
-loani_repository=`pwd|sed 's|/osdl/OSDL/trunk/src/conf/build||1'`
-#echo "loani_repository = $loani_repository"
-
-loani_installations=`dirname $loani_repository`/LOANI-installations
-#echo "loani_installations = $loani_installations"
-
-
-loani_repository_specified=1
-loani_installations_specified=1
-
-ceylan_location=""
 
 
 # 0 means true, 1 means false :
@@ -41,41 +51,41 @@ do_chain_tests=1
 do_only_prepare_dist=1
 
 
-while [ "$#" -gt "0" ] ; do
+while [ $# -gt 0 ] ; do
 	token_eaten=1
 	
-	if [ "$1" == "-v" ] || [ "$1" == "--verbose" ] ; then
+	if [ "$1" = "-v" -o "$1" = "--verbose" ] ; then
 		be_verbose=0
 		token_eaten=0
 	fi
 	
-	if [ "$1" == "-q" ] || [ "$1" == "--quiet" ] ; then
+	if [ "$1" = "-q" -o "$1" = "--quiet" ] ; then
 		be_quiet=0
 		token_eaten=0
 	fi
 	
-	if [ "$1" == "-d" ] || [ "$1" == "--disable-all-features" ] ; then
+	if [ "$1" = "-d" -o "$1" = "--disable-all-features" ] ; then
 		osdl_features_opt="$osdl_features_disable_opt"
 		token_eaten=0
 	fi
 
-	if [ "$1" == "-n" ] || [ "$1" == "--no-build" ] ; then
+	if [ "$1" = "-n" -o "$1" = "--no-build" ] ; then
 		do_stop_after_configure_generation=0
 		token_eaten=0
 	fi
 	
-	if [ "$1" == "-c" ] || [ "$1" == "--chain-test" ] ; then
+	if [ "$1" = "-c" -o "$1" = "--chain-test" ] ; then
 		do_chain_tests=0		
 		token_eaten=0
 	fi
 	
-	if [ "$1" == "-f" ] || [ "$1" == "--full-test" ] ; then
+	if [ "$1" = "-f" -o "$1" = "--full-test" ] ; then
 		do_chain_tests=0	
 		do_distcheck=1	
 		token_eaten=0
 	fi
 	
-	if [ "$1" == "-o" ] || [ "$1" == "--only-prepare-dist" ] ; then
+	if [ "$1" = "-o" -o "$1" = "--only-prepare-dist" ] ; then
 		do_build=1
 		do_check=1
 		do_install=1
@@ -86,59 +96,54 @@ while [ "$#" -gt "0" ] ; do
 		token_eaten=0
 	fi
 	
-	if [ "$1" == "--loani-repository" ] ; then
+	if [ "$1" = "--with-osdl-environment" ] ; then
 		shift
-		loani_repository_specified=0
-		loani_repository="$1"
-		if [ ! -d "${loani_repository}" ] ; then
-			echo -e "Error, specified LOANI repository directory does not exist ($loani_repository).\n$USAGE" 1>&2	
-			exit 5
+		osdl_environment_file="$1"
+		if [ ! -f "${osdl_environment_file}" ] ; then
+			echo "Error, specified OSDL environment file does not exist ($osdl_environment_file).
+$USAGE" 1>&2	
+			exit 4
 		fi
+		source ${osdl_environment_file}
 		token_eaten=0
 	fi
-	
-	if [ "$1" == "--loani-installations" ] ; then
-		shift
-		loani_installations_specified=0
-		loani_installations="$1"
-		if [ ! -d "${loani_installations}" ] ; then
-			echo -e "Error, specified LOANI installations directory does not exist ($loani_installations).\n$USAGE" 1>&2	
-			exit 6
-		fi
-		token_eaten=0
-	fi
-	
-	if [ "$1" == "--ceylan-install-prefix" ] ; then
+		
+	if [ "$1" = "--ceylan-install-prefix" ] ; then
 		shift
 		ceylan_location="$1"
 		if [ ! -d "${ceylan_location}" ] ; then
-			echo -e "Error, specified Ceylan installation directory does not exist ($ceylan_location).\n$USAGE" 1>&2	
+			echo "Error, specified Ceylan installation directory does not exist ($ceylan_location).
+$USAGE" 1>&2
 			exit 7
 		fi
 		token_eaten=0
 	fi
-	
-	if [ "$1" == "--configure-options" ] ; then
+		
+	if [ "$1" = "--configure-options" ] ; then
 		shift
-		configure_opt="$*"
+		configure_user_opt="$*"
 		while [ "$#" -gt "0" ] ; do
 			shift
 		done		
 		token_eaten=0
 	fi
 	
-	if [ "$1" == "-h" ] || [ "$1" == "--help" ] ; then
-		echo -e "$USAGE"
+	if [ "$1" = "-h" -o "$1" = "--help" ] ; then
+		echo "$USAGE"
 		exit
 		token_eaten=0
 	fi
 
-	if [ "$token_eaten" == "1" ] ; then
-		echo -e "Error, unknown argument ($1).\n$USAGE" 1>&2
-		exit 4
+	if [ $token_eaten -eq 1 ] ; then
+		echo "Error, unknown argument ($1).
+$USAGE" 1>&2
+		exit 5
 	fi	
 	shift
 done
+
+
+
 
 
 # debug mode activated iff equal to true (0) :
@@ -190,36 +195,122 @@ RUNNING_DIR=`pwd`
 SOURCE_OFFSET="../../.."
 
 
-# Prefix section for the OSDL installation.
+# Default value guessed from current path :
+loani_repository=`pwd|sed 's|/osdl/OSDL/trunk/src/conf/build||1'`
+#echo "loani_repository = $loani_repository"
 
-PREFIX_DEFAULT=$loani_installations
-debug "Default prefix = ${PREFIX_DEFAULT}"
+loani_installations=`dirname $loani_repository`/LOANI-installations
+#echo "loani_installations = $loani_installations"
 
-PREFIX_SECOND_DEFAULT="$HOME/tmp-OSDL-test-install"
 
-if [ ! -d `dirname ${PREFIX_DEFAULT}` ] ; then
-	echo "Base of first default install directory (${PREFIX_DEFAULT}) not existing, switching to second default directory (${PREFIX_SECOND_DEFAULT})"
-	PREFIX="${PREFIX_SECOND_DEFAULT}"
+if [ -z "$osdl_environment_file" ] ; then
+	
+	if [ -z "$ceylan_location" ] ; then
+
+		# Nothing specified, trying to guess where a OSDl-environment.sh file
+		# is to be found :
+
+	
+		osdl_environment_file=${loani_installations}/OSDL-environment.sh
+		
+		if [ ! -f "${osdl_environment_file}" ] ; then
+			echo "Error, guessed OSDL environment file not found (${osdl_environment_file}), please specify it.
+$USAGE" 1>&2		
+			exit 6
+		fi
+			
+		WARNING "Guessed OSDL environment file is ${osdl_environment_file}." 
+		. ${osdl_environment_file}
+
+		ceylan_location="$Ceylan_PREFIX"
+	
+		if [ ! -d "$ceylan_location" ] ; then
+	
+			echo "Error, Ceylan prefix retrieved thanks to guessed OSDL environment file ($osdl_environment_file) does not exist ($ceylan_location)" 1>&2
+			exit 7
+		
+		fi	
+	
+	fi
+
+	else 
+	
+		# ceylan_location specified and exists, ok.
+		
+		if [ $do_stop_after_configure_generation -eq 1 ] ; then
+			echo "Error, no OSDL environment file and full build requested nevertheless." 1>&2
+			exit 9
+		fi
+		
+	fi	
+	
 else
-	PREFIX="${PREFIX_DEFAULT}"
+
+	# osdl_environment_file specified, exists and has been sourced.
+	ceylan_location="$Ceylan_PREFIX"
+	
+	if [ ! -d "$ceylan_location" ] ; then
+	
+		echo "Error, Ceylan prefix retrieved thanks to specified OSDL environment file ($osdl_environment_file) does not exist ($ceylan_location)" 1>&2
+		exit 10
+	
+	fi
 fi
 
-#debug "Prefix = ${PREFIX}"
- 
-mkdir -p ${PREFIX}
+
+# Retrieving Ceylan substitute.sh script, needed by MakeConfigure :
+CEYLAN_SHARED_DIR=${ceylan_location}/share/Ceylan
+if [ ! -d "${CEYLAN_SHARED_DIR}" ] ; then
+	echo "Error, no Ceylan share directory found ($CEYLAN_SHARED_DIR)" 1>&2	
+	exit 11
+fi
+
+SUBSTITUTE_SCRIPT="${CEYLAN_SHARED_DIR}/scripts/shell/substitute.sh"
+if [ ! -x "${SUBSTITUTE_SCRIPT}" ] ; then
+	echo "Error, no executable substitute script found ($SUBSTITUTE_SCRIPT)" 1>&2	
+	exit 12
+fi
 
 
-if [ -n "${PREFIX}" ] ; then
-	PREFIX_OPT="--prefix=$PREFIX"
+if [ -n "${ceylan_location}" ] ; then
+	# Update accordingly the configure prefix for Ceylan macro CEYLAN_PATH :
+	configure_opt="${configure_opt} --with-ceylan-prefix=$ceylan_location"
+else
+	# The default install location when no prefix is used :
+	ceylan_location="/usr/local"
+fi
+
+#echo "ceylan_location = ${ceylan_location}"
+
+# If OSDL-environment.sh is used, we have to add the tools prefix :
+if [ -n "$osdl_environment_file" ] ; then
+
+	if [ -n "${SDL_PREFIX}" ] ; then
+		configure_opt="${configure_opt} --with-sdl-prefix=$SDL_PREFIX"
+	fi
+	
+fi
+
+
+# Prefix section for the OSDL installation.
+
+# If it is needed, then OSDL environment file is being used and OSDL_PREFIX
+# is already set.
+
+
+if [ -n "${OSDL_PREFIX}" ] ; then
+	mkdir -p ${OSDL_PREFIX}
+	PREFIX_OPT="--prefix=$OSDL_PREFIX"
 else
 	PREFIX_OPT=""
 fi
 
 
-if [ -z "${configure_opt}" ] ; then
-	configure_opt="$osdl_features_opt --enable-strict-ansi --enable-debug $PREFIX_OPT $test_overriden_options"
+if [ -n "${configure_user_opt}" ] ; then
+	configure_opt="$configure_user_opt"
+else	
+	configure_opt="$configure_opt $osdl_features_opt --enable-strict-ansi --enable-debug $PREFIX_OPT $test_overriden_options"
 fi
-
 
 
 # Log-on-file mode activated iff equal to true (0) :
@@ -235,73 +326,8 @@ fi
 debug "COMMAND = $COMMAND"
 debug "RUNNING_DIR = $RUNNING_DIR" 
 
-debug "loani_installations = $loani_installations"
-
-echo 
-
-if [ $loani_repository_specified -eq 1 ] ; then
-	if [ ! -d "${loani_repository}" ] ; then
-		echo -e "Error, no LOANI repository directory specified, and guessed one does not exist ($loani_repository).\n$USAGE" 1>&2	
-		exit 7
-	fi	
-		
-	warning "No directory specified for LOANI repository, defaulting to $loani_repository"
-	
-fi
 
 
-if [ $loani_installations_specified -eq 1 ] ; then
-	if [ ! -d "${loani_installations}" ] ; then
-		echo -e "Error, no LOANI installation directory specified, and guessed one does not exist ($loani_installations).\n$USAGE" 1>&2	
-		exit 8
-	fi	
-		
-	warning "No directory specified for LOANI installations, defaulting to $loani_installations"
-	
-fi
-
-
-# Guessing where Ceylan should be found :
-#   1. try under corresponding specified or guessed LOANI-installations 
-#   2. otherwise, look into standard locations (no specific option passed)
-
-if [ -z "${ceylan_location}" ] ; then
-
-	# Ignore elements of Makefile syntax :
-	source ../OSDLSettings.inc 2>/dev/null
-		ceylan_loanized_dir="${loani_installations}/Ceylan-${CEYLAN_OLDEST_SUPPORTED_MAJOR}.${CEYLAN_OLDEST_SUPPORTED_MINOR}"
-	
-	if [ -d "${ceylan_loanized_dir}" ] ; then
-		ceylan_location="${ceylan_loanized_dir}"
-	fi
-	
-fi
-
-
-if [ -n "${ceylan_location}" ] ; then
-	# Update accordingly the configure prefix for Ceylan macro CEYLAN_PATH :
-	configure_opt="${configure_opt} --with-ceylan-prefix=$ceylan_location"
-else
-	# The default install location when no prefix is used :
-	ceylan_location="/usr/local"
-fi
-
-#echo "ceylan_location = ${ceylan_location}"
-
-
-# Retrieving Ceylan substitute.sh script, needed by MakeConfigure :
-CEYLAN_SHARED_DIR=${ceylan_location}/share/Ceylan
-if [ ! -d "${CEYLAN_SHARED_DIR}" ] ; then
-	echo -e "Error, no Ceylan share directory found ($CEYLAN_SHARED_DIR)" 1>&2	
-	exit 9
-fi
-
-SUBSTITUTE_SCRIPT="${CEYLAN_SHARED_DIR}/scripts/shell/substitute.sh"
-if [ ! -x "${SUBSTITUTE_SCRIPT}" ] ; then
-	echo -e "Error, no executable substitute script found ($SUBSTITUTE_SCRIPT)" 1>&2	
-	exit 10
-fi
-	
 
 # Overall autotools settings :
 
@@ -334,7 +360,7 @@ execute()
 
 	echo "    Executing $*"
 	
-	if [ "$log_on_file" -eq 0 ] ; then
+	if [ $log_on_file -eq 0 ] ; then
 		echo "    Executing $* from "`pwd` >>"$log_filename"
 		eval $* >>"$log_filename" 2>&1
 		RES=$?
@@ -347,19 +373,29 @@ execute()
 
 	if [ ! $RES -eq 0 ] ; then
 		echo 1>&2
-		if [ "$log_on_file" -eq 0 ] ; then
+		if [ $log_on_file -eq 0 ] ; then
 			echo "Error while executing '$*', see $log_filename" 1>&2
 		else
 			echo "Error while executing '$*'" 1>&2
 			
-			AUTOMAKE_HINT="\nTo upgrade automake and aclocal from Debian-based distributions, do the following as root : 'apt-get install automake1.9' which updates aclocal too. One has nonetheless to update the symbolic links /etc/alternatives/aclocal so that it points to /usr/bin/aclocal-1.9, and /etc/alternatives/automake so that it points to /usr/bin/automake-1.9"
+			AUTOMAKE_HINT="
+To upgrade automake and aclocal from Debian-based distributions, do the following as root : 'apt-get install automake1.9' which updates aclocal too. One has nonetheless to update the symbolic links /etc/alternatives/aclocal so that it points to /usr/bin/aclocal-1.9, and /etc/alternatives/automake so that it points to /usr/bin/automake-1.9"
 			
-			if [ "$1" == "aclocal" ]; then
-				echo -e "\nNote : if aclocal is failing since AM_CXXFLAGS (used in configure.ac) 'cannot be found in library', then check that your aclocal version is indeed 1.9 or newer. For example, with Debian-based distributions, /usr/bin/aclocal is a symbolic link to /etc/alternatives/aclocal, which itself is a symbolic link which may or may not point to the expected aclocal version. Your version of $1 is :\n\t" `$1 --version` "\n\n\t" `/bin/ls -l --color $(type -p $1)` "${AUTOMAKE_HINT}"
-			elif [ "$1" == "automake" ]; then
-				echo -e "\nNote : check that your automake version is indeed 1.9 or newer. For example, with Debian-based distributions, /usr/bin/automake is a symbolic link to /etc/alternatives/automake, which itself is a symbolic link which may or may not point to the expected automake version. Your version of $1 is :\n\t" `$1 --version` "\n\n\t" `/bin/ls -l --color $(type -p $1)` "${AUTOMAKE_HINT}"
-			elif [ "$1" == "./configure" ]; then
-				echo -e "\nNote : check the following log :" `pwd`/config.log
+			if [ "$1" = "aclocal" ]; then
+				echo "
+Note : if aclocal is failing since AM_CXXFLAGS (used in configure.ac) 'cannot be found in library', then check that your aclocal version is indeed 1.9 or newer. For example, with Debian-based distributions, /usr/bin/aclocal is a symbolic link to /etc/alternatives/aclocal, which itself is a symbolic link which may or may not point to the expected aclocal version. Your version of $1 is :
+	" `$1 --version` "
+	
+	" `/bin/ls -l --color $(type -p $1)` "${AUTOMAKE_HINT}"
+			elif [ "$1" = "automake" ]; then
+				echo "
+Note : check that your automake version is indeed 1.9 or newer. For example, with Debian-based distributions, /usr/bin/automake is a symbolic link to /etc/alternatives/automake, which itself is a symbolic link which may or may not point to the expected automake version. Your version of $1 is :
+	" `$1 --version` "
+
+	" `/bin/ls -l --color $(type -p $1)` "${AUTOMAKE_HINT}"
+			elif [ "$1" = "./configure" ]; then
+				echo "
+Note : check the following log :" `pwd`/config.log
   			fi
 			
 		fi
@@ -386,9 +422,9 @@ generateCustom()
 	fi
 	
 	
-	if [ "$do_clean_prefix" -eq 0 ] ; then
+	if [ $do_clean_prefix -eq 0 ] ; then
 		echo
-		if [ -z "$PREFIX" -o "$PREFIX" == "$HOME" ] ; then
+		if [ -z "$PREFIX" -o "$PREFIX" = "$HOME" ] ; then
 			echo "(no PREFIX=$PREFIX removed)"
 		else	
 			returnedChar="y"
@@ -396,7 +432,7 @@ generateCustom()
 				read -p "Do you really want to erase the whole OSDL tree in $PREFIX ? (y/n) [n] " returnedChar 
 			fi
 			
-			if [ "$returnedChar" == "y" ] ; then
+			if [ "$returnedChar" = "y" ] ; then
 				echo " - cleaning PREFIX = $PREFIX"
 				${RM} -rf $PREFIX/include/OSDL $PREFIX/lib/libOSDL* $PREFIX/share/OSDL* 
 				echo "(prefix cleaned)"
@@ -468,13 +504,13 @@ generateCustom()
 		exit 22
 	}
 
+	# Contains all *.m4 prerequesites, including ceylan.m4, sdl.m4, etc. :
 	M4_DIR=${CONFIG_DIR}/m4 
-	M4_CEYLAN_DIR=${CEYLAN_SHARED_DIR}
 	
 	ACLOCAL_OUTPUT=src/conf/build/m4/aclocal.m4
 	
 	# Do not use '--acdir=.' since it prevents aclocal from writing its file :
-	execute aclocal -I ${M4_DIR} -I ${M4_CEYLAN_DIR} --output=$ACLOCAL_OUTPUT $force $verbose
+	execute aclocal -I ${M4_DIR} --output=$ACLOCAL_OUTPUT $force $verbose
 
 	# automake wants absolutely to find aclocal.m4 in the top-level directory :
 	ln -sf src/conf/build/m4/aclocal.m4
@@ -542,21 +578,21 @@ generateCustom()
  	execute ./configure $configure_opt
 	
 
-	if [ "$do_clean" -eq 0 ] ; then
+	if [ $do_clean -eq 0 ] ; then
 		echo
 		echo " - cleaning all"
 	 	execute make clean
 	fi
 	
 	
-	if [ "$do_build" -eq 0 ] ; then
+	if [ $do_build -eq 0 ] ; then
 		echo
 		echo " - building all"
 	 	execute make
 	fi
 	
 	
-	if [ "$do_check" -eq 0 ] ; then
+	if [ $do_check -eq 0 ] ; then
 		echo
 		echo " - checking all"
 	 	execute make check
@@ -570,21 +606,21 @@ generateCustom()
 	fi
 	
 	
-	if [ "$do_installcheck" -eq 0 ] ; then
+	if [ $do_installcheck -eq 0 ] ; then
 		echo
 		echo " - checking install"
 	 	execute make installcheck
 	fi
 	
 	
-	if [ "$do_distcheck" -eq 0 ] ; then
+	if [ $do_distcheck -eq 0 ] ; then
 		echo
 		echo " - making distcheck"
 	 	execute make distcheck
 	fi
 	
 	
-	if [ "$do_chain_tests" -eq 0 ] ; then
+	if [ $do_chain_tests -eq 0 ] ; then
 		echo
 		echo " - building and running test suite"
 		cd test
