02e65ce82d006486d980ade42ddb1c010989bc93
Olivier Boudeville <olivier.boudeville@esperide.com>
2007-01-16 21:27:55 +0000

    Updated with autotools-based version management.

--- src/code/basic/OSDLDocMainPage.h
+++ src/code/basic/OSDLDocMainPage.h
@@ -1,31 +1,40 @@
 
-/*! 
+/**
  *
- *\mainpage OSDL
+ * \mainpage OSDL
  *
  * \section intro Introduction
  * 
  * <div class="box">
- * OSDL stands for Object-oriented SDL (Simple DirectMedia Layer) [<a href="http://osdl.sourceforge.net" target="_blank">official site</a>, <a href="http://osdl.esperide.com" target="_blank">mirror</a>].
+ * OSDL stands for Object-oriented SDL (Simple DirectMedia Layer) 
+ * [<a href="http://osdl.sourceforge.net" target="_blank">official site</a>, 
+ * <a href="http://osdl.esperide.com" target="_blank">mirror</a>].
  *
- * Basically, OSDL is a high-level library, working on top of low-level back-ends, such as SDL.
+ * Basically, OSDL is a high-level library, working on top of lowers-level
+ * back-ends, currently SDL (only).
  *
  * OSDL is a state machine.
  *
- * This is OSDL version 0.3 (pre-version).
+ * This is OSDL version @OSDL_VERSION@.
  *
- * When multiple ways of processing exist (for example, if there is an SDL fonction for drawing a line and if OSDL can do it too, its own way), you can choose to use OSDL's proper algorithms by defining OSDL_INSIDE
+ * When multiple ways of processing exist (for example, if there is an SDL
+ * fonction for drawing a line and if OSDL can do it too, its own way), 
+ * you can choose to use OSDL's proper algorithms by defining OSDL_INSIDE
  * (example : <CODE>CFLAGS += -DOSDL_INSIDE</CODE>)
  * </div>
  *
  * \section doc Documentation
  *
  * <div class="box">
- * Please refer to the <a href="../../main/documentation/OSDL/OSDL-0.3/OSDL-usersguide.html">library short manual</a> (user's guide). 
+ * Please refer to the 
+ * <a href="../../OSDL-@OSDL_VERSION@/index.html">OSDL user's guide</a>. 
  *
- * All SDL-related informations are to be found in <a href="http://www.libsdl.org/index.php">SDL Official Site</a>.
+ * All SDL-related informations are to be found in 
+ * <a href="http://www.libsdl.org/index.php">SDL Official Site</a>.
  *
- * For any remark or suggestion, <a href="mailto:olivier.boudeville@online.fr?subject=[OSDL]%20">feel free to drop me a line</a>, I like receiving mail !
+ * For any remark or suggestion, 
+ * <a href="mailto:olivier.boudeville@online.fr?subject=[OSDL]%20Remark">feel free 
+ *to drop me a line</a>.
  * </div> 
  *
  */
