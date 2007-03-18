36b0d53a8b069f7ab177f3ce24afa61eb5c31c1c
Olivier Boudeville <olivier.boudeville@esperide.com>
2007-03-18 18:03:56 +0000

    Comments updated. Stop tick correctly managed, more infos given.

--- test/engine/testOSDLSchedulerNoDeadline.cc
+++ test/engine/testOSDLSchedulerNoDeadline.cc
@@ -1,6 +1,7 @@
 #include "OSDL.h"
 using namespace OSDL ;
 using namespace OSDL::Events ;
+using namespace OSDL::Video ;
 using namespace OSDL::Engine ;
 
 
@@ -23,8 +24,7 @@ using std::list ;
  * last, including running it with various machine loads to check it does 
  * not change anything on the result (still no skip, only an extended duration).
  *
- *
- * @see testOSDLScheduler.cc
+ * @see testOSDLScheduler.cc for the soft realtime version.
  *
  */
  
@@ -42,9 +42,12 @@ class SchedulerStopper : public OSDL::Engine::ActiveObject
 	public:
 	
 	
-		SchedulerStopper( SimulationTick stopSimulationTick ) 
+		SchedulerStopper( SimulationTick stopSimulationTick, 
+			bool verbose = false ) 
 				throw( SchedulingException ) :
-			ActiveObject( stopSimulationTick, /* absolutlyDefined */ true ) 
+			ActiveObject( stopSimulationTick, /* absolutlyDefined */ true ), 
+			_stopTick( stopSimulationTick ),
+			_verbose( verbose ) 
 		{
 
 			// Be also a periodic object :
@@ -52,10 +55,14 @@ class SchedulerStopper : public OSDL::Engine::ActiveObject
 			const Hertz desiredFrequency = 10 ;
 			
 			Hertz obtainedFrequency = setFrequency( desiredFrequency ) ;
-			LogPlug::info( "SchedulerStopper constructor : "
-				"for a desired activation frequency of "
-				+ Ceylan::toString( desiredFrequency ) + " Hz, obtained "
-				+ Ceylan::toString( obtainedFrequency ) + " Hz." ) ;
+			
+			if ( _verbose )
+				LogPlug::info( "SchedulerStopper constructor : "
+					"for a desired activation frequency of "
+					+ Ceylan::toString( desiredFrequency ) + " Hz, obtained "
+					+ Ceylan::toString( obtainedFrequency ) 
+					+ " Hz. Will stop at simulation tick #"
+					+ Ceylan::toString( stopSimulationTick ) + "."  ) ;
 			
 			// This active object registers itself to the scheduler.
 			Scheduler::GetExistingScheduler().registerObject( * this ) ;
@@ -64,11 +71,13 @@ class SchedulerStopper : public OSDL::Engine::ActiveObject
 		
 		virtual void onActivation( Events::SimulationTick newTick ) throw()
 		{
-			LogPlug::info( "SchedulerStopper::onActivation : "
-				"activated for simulation tick "
-				+ Ceylan::toString( newTick ) + "." ) ;
+		
+			if ( _verbose )
+				LogPlug::info( "SchedulerStopper::onActivation : "
+					"activated for simulation tick "
+					+ Ceylan::toString( newTick ) + "." ) ;
 			
-			if ( newTick == 200 )
+			if ( newTick == _stopTick )
 			{	
 				LogPlug::info( "SchedulerStopper::onActivation : "
 					"stopping scheduler." ) ;
@@ -84,6 +93,7 @@ class SchedulerStopper : public OSDL::Engine::ActiveObject
 				+ Ceylan::toString( newTick ) + " had been skipped !" ) ;
 				
 			onActivation( newTick ) ;
+			
 		}
 		
 		
@@ -99,17 +109,22 @@ class SchedulerStopper : public OSDL::Engine::ActiveObject
 		}
 		
 		
+	private:
+	
+		SimulationTick _stopTick ;
+		bool _verbose ;
+		
 } ;
 
 
 
 /**
- * Testing the services of the OSDL scheduler for active objects.
- *
+ * Testing the services of the OSDL scheduler for active objects, in batch
+ * mode.
+ * 
  * @see ActiveObject
  *
  */
-
 int main( int argc, char * argv[] ) 
 {
 
@@ -120,7 +135,70 @@ int main( int argc, char * argv[] )
 	{
 
 		
-		LogPlug::info( "Testing OSDL scheduler services." ) ;
+		LogPlug::info( "Testing OSDL scheduler services in batch mode." ) ;
+
+		// Tells when the test will stop, by default after 30s (100 Hz) :
+		Events::SimulationTick stopTick = 30 * 100 ;
+
+
+		bool isBatch = false ;
+		
+		std::string executableName ;
+		std::list<std::string> options ;
+		
+		Ceylan::parseCommandLineOptions( executableName, options, argc, argv ) ;
+		
+		std::string token ;
+		bool tokenEaten ;
+		
+		
+		while ( ! options.empty() )
+		{
+		
+			token = options.front() ;
+			options.pop_front() ;
+
+			tokenEaten = false ;
+						
+			if ( token == "--batch" )
+			{
+				LogPlug::info( "Batch mode selected" ) ;
+				isBatch = true ;
+				
+				/*
+				 * Will stop the scheduler after 1 second 
+				 * (100 simulation ticks, since logic frequency is 100 Hz here).
+				 *
+				 */
+				stopTick = 100 ;
+				
+				tokenEaten = true ;
+			}
+			
+			if ( token == "--interactive" )
+			{
+				LogPlug::info( "Interactive mode selected" ) ;
+				isBatch = false ;
+				tokenEaten = true ;
+			}
+			
+			
+			if ( LogHolder::IsAKnownPlugOption( token ) )
+			{
+				// Ignores log-related (argument-less) options.
+				tokenEaten = true ;
+			}
+			
+			
+			if ( ! tokenEaten )
+			{
+				throw Ceylan::CommandLineParseException( 
+					"Unexpected command line argument : " + token ) ;
+			}
+		
+		}
+		
+
 
 		LogPlug::info( 
 			"Starting OSDL with video and, therefore, events enabled." ) ;	
@@ -135,28 +213,22 @@ int main( int argc, char * argv[] )
 
 		LogPlug::info( EventsModule::DescribeEnvironmentVariables() ) ;
 				
-		LogPlug::info( "Displaying a dummy window to have access "
-			"to an event queue." ) ;
+		LogPlug::info( "Displaying a dummy window "
+			"to have access to an event queue." ) ;
 			
 		LogPlug::info( "Getting video." ) ;
 		OSDL::Video::VideoModule & myVideo = myOSDL.getVideoModule() ; 
 		
 		// A SDL window is needed to have the SDL event system working :
-		myVideo.setMode( 640, 480, 16, 
-			OSDL::Video::VideoModule::SoftwareSurface ) ;
+		myVideo.setMode( 640, 480, VideoModule::UseCurrentColorDepth, 
+			VideoModule::SoftwareSurface ) ;
 		
-		LogPlug::info( "Ask for the scheduler to be used." ) ;
+		LogPlug::info( "Asking for a scheduler to be used." ) ;
 		myEvents.useScheduler() ;
 		
 		Scheduler::GetExistingScheduler().setScreenshotMode( true, 
 			/* frameFilenamePrefix */ "testOSDLSchedulerNoDeadline-" ) ;
 			
-		/*
-		 * Will stop the scheduler after 2 seconds 
-		 * (200 simulation ticks, since logic frequency is 100 Hz here).
-		 *
-		 */
-		SimulationTick stopTick = 200 ;
 		LogPlug::info( "Create an active object whose role is "
 			"to stop the scheduler at simulation tick " 
 			+ Ceylan::toString( stopTick ) + "." ) ;
@@ -165,20 +237,20 @@ int main( int argc, char * argv[] )
 		
 		list<SchedulerStopper *> stoppers ;
 		WhiteNoiseGenerator stopTickRand( 0, stoppersCount ) ;
-		for ( Ceylan::Uint32 i = 0; i < stoppersCount; i++ )
+
+		stoppers.push_back( new SchedulerStopper( 
+				stopTick + stopTickRand.getNewValue(), /* verbose */ true ) ) ;
+		
+		for ( Ceylan::Uint32 i = 1; i < stoppersCount; i++ )
 		{
 			// All stoppers will stop at simulation tick 200 or later :
 			stoppers.push_back( new SchedulerStopper( 
-				200 + stopTickRand.getNewValue() ) ) ;		
+				stopTick + stopTickRand.getNewValue() ) ) ;		
 		}
 		
 				
-		LogPlug::debug( "After object registering : " 
-			+ Scheduler::GetExistingScheduler().toString( Ceylan::high ) ) ;
-			
 		LogPlug::info( "Entering the schedule loop." ) ;
 		myEvents.enterMainLoop() ;
-		
 		LogPlug::info( "Exit from schedule loop." ) ;		
 		
 		for ( list<SchedulerStopper *>::iterator it = stoppers.begin(); 
