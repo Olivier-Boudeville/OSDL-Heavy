572ee28e45561ccd30b05e2ba0dcc6ca5433ec4d
Olivier Boudeville <olivier.boudeville@esperide.com>
2007-04-21 21:56:35 +0000

    Helps cleaning the Microsoft mess.

---
+++ src/code/basic/OSDLIncludeCorrecter.h
@@ -0,0 +1,23 @@
+/*
+ * No include guards wanted : to be included each time defines 
+ * can be screwed up.
+ *
+ */
+
+#ifdef OSDL_RUNS_ON_WINDOWS
+
+// Microsoft stupidly managed to redefine symbols in a header (winuser.h) :
+
+#ifdef LoadIcon
+#undef LoadIcon
+#endif // LoadIcon
+
+#ifdef LoadImage
+#undef LoadImage
+#endif // LoadImage
+
+#ifdef LoadImageA
+#undef LoadImageA
+#endif // LoadImageA
+
+#endif // OSDL_RUNS_ON_WINDOWS
