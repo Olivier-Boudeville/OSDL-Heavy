d90332d428be88410db79fcec150cc46de7d5769
Olivier Boudeville <olivier.boudeville@esperide.com>
2007-11-02 09:34:30 +0000

    Uniformized with Ceylan counterpart. More details in test log output.

--- test/playTests.sh
+++ test/playTests.sh
@@ -4,7 +4,7 @@ TESTLOGFILE=`pwd`"/testsOutcome.txt"
 PLAYTEST_LOCAL_FILE="playTests-local.sh"
 
 
-USAGE="`basename $0` [--interactive] : executes all tests for OSDL in a row.
+USAGE="`basename $0` [--interactive]: executes all tests for OSDL in a row.
 	
 	If the --interactive option is used, tests will not be run in batch mode, and will prompt the user for various inputs. Otherwise only their final result will be output. In all cases their messages will be stored in file ${TESTLOGFILE}. The return code of this script will be the number of failed tests (beware to overflow of the return code)"
 
@@ -12,24 +12,24 @@ USAGE="`basename $0` [--interactive] : executes all tests for OSDL in a row.
 # Please remember, when debugging on UNIX playTests.sh, to execute it from
 # *installed* version, but to modify the playTests.sh from *source* code, 
 # and to copy back the latter to the former.
- 
 
-# In batch (non-interactive) mode by default (0) :
+
+# In batch (non-interactive) mode by default (0):
 is_batch=0
 
 
 
 if [ "$#" -ge "2" ] ; then
 	echo "
-	Usage : $USAGE" 1>&2
+	Usage: $USAGE" 1>&2
 	exit 1
 fi	
 
 if [ "$#" -eq 1 ] ; then
 	if [ "$1" != "--interactive" ] ; then
-		echo "$1 : unknown option." 1>&2
+		echo "$1: unknown option." 1>&2
 		echo "
-		Usage : $USAGE" 1>&2
+		Usage: $USAGE" 1>&2
 		exit 2
 	else
 		is_batch=1	
@@ -45,21 +45,21 @@ debug_mode=1
 DEBUG_INTERNAL()
 # Prints debug informations if debug mode is on.
 {
-	[ "$debug_mode" = 1 ] || echo "Debug : $*"
+	[ "$debug_mode" = 1 ] || echo "Debug: $*"
 }
 
 
 WARNING_INTERNAL()
 # Prints warning informations to error output.
 {
-	echo "Warning : $*" 1>&2
+	echo "Warning: $*" 1>&2
 }
 
 
 ERROR_INTERNAL()
 # Prints error informations to error output.
 {
-	echo "Error : $*" 1>&2
+	echo "Error: $*" 1>&2
 }
 
 
@@ -67,10 +67,10 @@ DEBUG_INTERNAL "Debug mode activated"
 
 
 
-# Some useful test functions :
+# Some useful test functions:
 
 display_launching()
-# Usage : display_launching <name of the test>
+# Usage: display_launching <name of the test>
 {
 
 	test_name="$1"
@@ -84,8 +84,9 @@ display_launching()
 }
 
 
+
 display_test_result()
-# Usage : display_test_result <name of the test> <test path> <returned code>
+# Usage: display_test_result <name of the test> <test path> <returned code>
 {
 
 	test_name="$1"
@@ -93,7 +94,7 @@ display_test_result()
 	return_code="$3"
 	
 	if [ "$return_code" -eq 0 ] ; then
-		# Test succeeded :
+		# Test succeeded:
 		if [ $is_batch -eq 1 ] ; then
 			echo
 			printColor "${term_offset}$test_name seems to be successful     " $green_text $black_back
@@ -103,7 +104,7 @@ display_test_result()
 				
 	else
 			
-		# Test failed :
+		# Test failed:
 		error_count=`expr $error_count + 1`
 		if [ $is_batch -eq 1 ] ; then
 			echo
@@ -113,7 +114,7 @@ display_test_result()
 			if [ "$check_dependency" -eq 0 ] ; then
 			
 				if [ "$on_cygwin" -eq 0 ] ; then
-					# See also : http://www.dependencywalker.com/
+					# See also: http://www.dependencywalker.com/
 					PATH="/cygdrive/c/Program Files/Microsoft Platform SDK for Windows Server 2003 R2/bin:$PATH"
 					depend_tool="Depends.exe"
 					if which $depend_tool 1>/dev/null 2>&1; then
@@ -124,7 +125,7 @@ display_test_result()
 					fi
 				else
 					if [ $has_ldd -eq 0 ] ; then
-						echo "$t failed, whose shared library dependencies are : " >> ${TESTLOGFILE}
+						echo "$t failed, whose shared library dependencies are: " >> ${TESTLOGFILE}
 						${ldd_tool} $t >>${TESTLOGFILE}
 					else
 						echo "$t failed." >> ${TESTLOGFILE}
@@ -138,8 +139,9 @@ display_test_result()
 }
 
 
+
 run_test()
-# Usage : run_test <name of the test> <test path> 
+# Usage: run_test <name of the test> <test path> 
 {
 
 	test_name="$1"
@@ -147,48 +149,68 @@ run_test()
 	
 	# The --interactive parameter is used to tell the test it is 
 	# run in interactive mode, so that those which are long 
-	# (ex : stress tests) are shorten.
+	# (ex: stress tests) are shorten.
+
+	echo "
+		
+	########### Running now $t" >>${TESTLOGFILE}
+		
+	if [ $has_ldd -eq 0 ] ; then
+		echo "Library dependencies: " >>${TESTLOGFILE}
+		${ldd_tool} $t >>${TESTLOGFILE}
+	fi
+
 	if [ $is_batch -eq 0 ] ; then
-		echo "
 		
-		########### Running now $t" >>${TESTLOGFILE}
+		echo "Command line: $t --batch ${network_option} ${log_plug_option}" >>${TESTLOGFILE}
 		
-		if [ $has_ldd -eq 0 ] ; then
-			echo "Library dependencies : " >>${TESTLOGFILE}
-			${ldd_tool} $t >>${TESTLOGFILE}
-		fi
-		
-		echo "Command line : $t --batch ${network_option} ${log_plug_option}" >>${TESTLOGFILE}
 		$t --batch ${network_option} ${log_plug_option} 1>>${TESTLOGFILE} 2>&1
+		
 	else
+	
+		echo "Command line: $t --interactive ${network_option} ${log_plug_option}" >>${TESTLOGFILE}
 		$t --interactive ${network_option} ${log_plug_option}
+		
 	fi			
 		
 	return_code="$?"
 	
 	display_test_result "$test_name" "$t" "$return_code"
 	
+	
+	if [ $return_code -eq 0 ] ; then
+		echo "Test $t succeeded." 1>>${TESTLOGFILE}		
+	else
+		echo "Test $t failed with return code $return_code." 1>>${TESTLOGFILE} 
+	fi
+		
 		
 	if [ $is_batch -eq 1 ] ; then
+
 		printColor "${term_primary_marker}End of $test_name, press enter to continue" $cyan_text $blue_back
 		read 
 		clear
+
 	fi		
 			
 }
 
 
+
 display_final_stats()
 {
+
 	echo 
 
 	if [ "$error_count" -eq 0 ] ; then
-		echo "   Test result : [${green_text}m all $test_count tests succeeded[${white_text}m"
+		echo "   Test result: [${green_text}m all $test_count tests succeeded[${white_text}m"
 	else
-		echo "   Test result : [${red_text}m $error_count out of $test_count tests failed[${white_text}m"
+		echo "   Test result: [${red_text}m $error_count out of $test_count tests failed[${white_text}m"
 		echo "   (see ${TESTLOGFILE} for more details)"
 	fi
+	
 }
+
 
 
 get_logical_test_name()
@@ -205,9 +227,10 @@ get_logical_test_name()
 }
 
 
-# Try to find automagically the relevant OSDL-environment.sh file :
 
-# If absolute path given :
+# Try to find automagically the relevant OSDL-environment.sh file:
+
+# If absolute path given:
 first_char=`echo $0 | head -c 1`
 if [ "$first_char" = "/" ] ; then
 	starting_dir=`dirname $0`
@@ -218,7 +241,7 @@ fi
 
 #echo "starting_dir = $starting_dir"
 
-# Suppose we are in the build tree : 
+# Suppose we are in the build tree: 
 loani_installations=`echo $starting_dir | sed 's|osdl/OSDL/trunk/test||1'`"../LOANI-installations"
 	
 #echo "loani_installations = $loani_installations"
@@ -250,11 +273,11 @@ fi
 . $osdl_environment_file
 
 	
-# Try to find term utilities :
+# Try to find term utilities:
 
 TEST_ROOT=`dirname $0`
 
-# Not using Cygwin by default to chain the tests :
+# Not using Cygwin by default to chain the tests:
 on_cygwin=1
 
 if [ `uname -s | cut -b1-6` = "CYGWIN" ] ; then
@@ -268,7 +291,7 @@ else
 fi
 
 
-# Triggers also termUtils.sh and platformDetection.sh :
+# Triggers also termUtils.sh and platformDetection.sh:
 DEFAULT_LOCATIONS_PATH="$SHELLS_LOCATION/defaultLocations.sh"
 
 if [ -f "$DEFAULT_LOCATIONS_PATH" ] ; then
@@ -278,17 +301,17 @@ else
 	exit 3
 fi	
 
-# For ping :
+# For ping:
 findSupplementaryShellTools
 
 
-# Creates a test directory to avoid polluting other directories :
+# Creates a test directory to avoid polluting other directories:
 TEST_DIR="tests-results-"`date '+%Y%m%d'`
 
 
 # Specifies the log plug the tests should be run with. 
-# Note : avoid using the classical plug because it may cause scheduling 
-# failures (ex : with testOSDLScheduler) because of its default synchronous 
+# Note: avoid using the classical plug because it may cause scheduling 
+# failures (ex: with testOSDLScheduler) because of its default synchronous 
 # file I/O). 
 log_plug_option="--HTMLPlug"
 
@@ -301,7 +324,7 @@ else
 	Interactive tests will need an input device (joystick, mouse, keyboard) to be used. Be warned though that some tests might take a long time, and that some of them have no special output except a test result."
 fi
 
-# Test whether we are online (needed for DNS queries) :
+# Test whether we are online (needed for DNS queries):
 if ${PING} ${PING_OPT} 2 google.com 1>/dev/null 2>&1; then
 	is_online=0
 	network_option="--online"
@@ -319,19 +342,19 @@ test_count=0
 error_count=0
 
 
-# Tells whether link dependencies should be checked in case a test fails :
+# Tells whether link dependencies should be checked in case a test fails:
 check_dependency=1
 
-# Special case for tests generated on Windows :
+# Special case for tests generated on Windows:
 if [ "$on_cygwin" -eq 0 ] ; then
 	
 	on_cygwin=0
 	DEBUG_INTERNAL "Running tests in the Windows (Cygwin) context."
 	
-	# Tests with LOANI are by default run against the debug multithread libraries :
+	# Tests with LOANI are by default run against the debug multithread libraries:
 	libraries_from_loani_tested_flavour="debug-mt"
 	
-	# Updated PATH needed to find the LOANI-built DLL :
+	# Updated PATH needed to find the LOANI-built DLL:
 	export PATH="$loani_installations/OSDL-libraries/${libraries_from_loani_tested_flavour}/dll:$PATH"
 	
 fi	
@@ -339,10 +362,10 @@ fi
 # This script will automatically run each test of each selected OSDL module.
 TESTED_ROOT_MODULES=`cd ${TEST_ROOT}; find . -type d | grep -v cross-tests | grep -v tmp | grep -v Debug | grep -v autom4te.cache | grep -v .svn | grep -v '.deps' | grep -v '.libs' | grep -v '^test'  | grep -v '/test' | grep -v '.exe-logs' | grep -v '^\.$'`
 
-DEBUG_INTERNAL "Tested modules are : ${TESTED_ROOT_MODULES}"
+DEBUG_INTERNAL "Tested modules are: ${TESTED_ROOT_MODULES}"
 
 
-# "[OK] " is 5 character wide and must be aligned along the right edge :
+# "[OK] " is 5 character wide and must be aligned along the right edge:
 if [ -z "${COLUMNS}" ] ; then
 	DEBUG_INTERNAL "Retrieving columns thanks to tput"	
 	COLUMNS=`tput cols`
@@ -365,11 +388,11 @@ fi
 if [ "$on_cygwin" -eq 0 ] ; then
 	echo "
 	
-	Library search path is : PATH='$PATH'" >> ${TESTLOGFILE}
+	Library search path is: PATH='$PATH'" >> ${TESTLOGFILE}
 else
 	echo "
 	
-	Library search path is : LD_LIBRARY_PATH='$LD_LIBRARY_PATH'" >> ${TESTLOGFILE}
+	Library search path is: LD_LIBRARY_PATH='$LD_LIBRARY_PATH'" >> ${TESTLOGFILE}
 fi
 
 has_ldd=1
@@ -379,7 +402,7 @@ if [ -x "${ldd_tool}" ] ; then
 	has_ldd=0
 fi
 
-# So that test plugin can be found :
+# So that test plugin can be found:
 saved_LTDL_LIBRARY_PATH="$LTDL_LIBRARY_PATH"
 
 
@@ -396,7 +419,7 @@ for m in ${TESTED_ROOT_MODULES} ; do
 	PLAYTEST_LOCAL="${TEST_ROOT}/$m/${PLAYTEST_LOCAL_FILE}"
 	
 	printColor "
-	${term_offset}${term_primary_marker}Playing all tests of module '"`echo $m | sed 's|^./||1'`"' : " $magenta_text $black_back
+	${term_offset}${term_primary_marker}Playing all tests of module '"`echo $m | sed 's|^./||1'`"': " $magenta_text $black_back
 	
 	if [ -f "${PLAYTEST_LOCAL}" ] ; then
 		EXCLUDED_TESTS=""
@@ -410,11 +433,11 @@ for m in ${TESTED_ROOT_MODULES} ; do
 		TESTS=`ls ${TEST_ROOT}/$m/testOSDL*.exe 2>/dev/null`	
 	fi
 	
-	DEBUG_INTERNAL "Tests in module ${m} are : '${TESTS}'"
+	DEBUG_INTERNAL "Tests in module ${m} are: '${TESTS}'"
 	
 	if [ $is_batch -eq 1 ] ; then
 	
-		# Lists all tests that are to be run :
+		# Lists all tests that are to be run:
 		for t in $TESTS ; do
 			if [ -x "$t" -a -f "$t" ] ; then
 				printColor "${term_offset}${term_offset}+ `basename $t`" $cyan_text $black_back
@@ -442,7 +465,7 @@ for m in ${TESTED_ROOT_MODULES} ; do
 			done	
 		
 			if [ "$to_be_excluded" -eq 0 ] ; then
-				# Skip this test, as PLAYTEST_LOCAL_FILE took care of it :
+				# Skip this test, as PLAYTEST_LOCAL_FILE took care of it:
 				DEBUG_INTERNAL "Skipping test $t"
 				continue
 			fi
