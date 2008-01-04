d5bbf538a9b91e890494f41ab79000b26c7b8f92
Olivier Boudeville <olivier.boudeville@esperide.com>
2008-01-04 22:25:58 +0000

    Some Ceylan defines added for direct inclusions.

--- src/code/OSDLConfigForNintendoDS.h
+++ src/code/OSDLConfigForNintendoDS.h
@@ -10,8 +10,13 @@
  *
  * Not related in any way to autoconf.
  *
+ * Some Ceylan symbols are defined, so that direct includes of Ceylan C header
+ * can be performed.
+ *
  */
 
+#define CEYLAN_ARCH_NINTENDO_DS 1
+ 
  
 // Auto-set the arch flags expected by libnds:
 #ifdef OSDL_RUNS_ON_ARM7
@@ -20,11 +25,15 @@
 	
 	#define OSDL_DS_LOG(messageString)
 	
+	#define CEYLAN_RUNS_ON_ARM7 1
+	
 #else // OSDL_RUNS_ON_ARM7
 
 	#ifdef OSDL_RUNS_ON_ARM9
 
 		#define ARM9
+
+		#define CEYLAN_RUNS_ON_ARM9 1
 	
 		// For iprintf and al:
 		#include <stdio.h> 
@@ -54,6 +63,7 @@
 #define OSDL_USES_SDL_GFX 0
 #define OSDL_USES_SDL_IMAGE 0
 #define OSDL_USES_SDL_TTF 0
+
 
  
 #endif // OSDL_CONFIG_FOR_NINTENDO_DS
