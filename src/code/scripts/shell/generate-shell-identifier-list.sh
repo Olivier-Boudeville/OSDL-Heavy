d889b8d98f6b8c01ae1cc327cfd604dbe414bcb0
Olivier Boudeville <olivier.boudeville@esperide.com>
2008-03-14 20:55:17 +0000

    Auto-generation of identifier files.

---
+++ src/code/scripts/shell/generate-shell-identifier-list.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+USAGE=`basename $0`" <identifier text file>: generates from specified text file X.txt containing an identifier per line (ex: 'HelloWorld' in line 27) a X.sh file ready to be sourced by shell scripts containing numerical identifiers corresponding to the line of the identifier (ex: 'HelloWorld=27')"
+
+
+SOURCE_FILE="$1"
+
+if [ -z "${SOURCE_FILE}" ]; then
+	echo "
+	Error, no source identifier file specified. 
+	Usage: $USAGE." 1>&2
+	exit 1
+fi
+
+
+if [ ! -f "${SOURCE_FILE}" ]; then
+	echo "
+	Error, specified source identifier file (${SOURCE_FILE}) is not an existing file. 
+	Usage: $USAGE." 1>&2
+	exit 2
+fi
+
+TARGET_FILE=`echo ${SOURCE_FILE}|sed 's|.txt$|.sh|1'`
+
+#echo "TARGET_FILE = ${TARGET_FILE}"
+
+# This appends to each line '=' and the number of that line:
+cat ${SOURCE_FILE} | sort | uniq | sed = | sed 'N;s|\n| = |' | awk '{printf "%-30s =  %s\n", $3, $1}' > ${TARGET_FILE} && echo "
+	File ${TARGET_FILE} generated ! "
+	
+	
