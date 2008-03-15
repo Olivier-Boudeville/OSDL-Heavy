2be19342a173072b6c1ab5f362f1282400ef6aa8
Olivier Boudeville <olivier.boudeville@esperide.com>
2008-03-15 19:57:36 +0000

    generate-shell-identifier-list.sh renamed to generate-identifier-list.sh.

--- src/code/scripts/shell/generate-identifier-list.sh
+++ src/code/scripts/shell/generate-identifier-list.sh
@@ -20,12 +20,23 @@ if [ ! -f "${SOURCE_FILE}" ]; then
 	exit 2
 fi
 
-TARGET_FILE=`echo ${SOURCE_FILE}|sed 's|.txt$|.sh|1'`
+
+if [ -z "$2" ] ; then
+	# Choose the name of produced file:
+	TARGET_FILE=`echo ${SOURCE_FILE}|sed 's|.txt$|.id|1'`
+else
+	# Use specified one:
+	TARGET_FILE=$2
+fi
 
 #echo "TARGET_FILE = ${TARGET_FILE}"
 
 # This appends to each line '=' and the number of that line:
-cat ${SOURCE_FILE} | sort | uniq | sed = | sed 'N;s|\n| = |' | awk '{printf "%-30s =  %s\n", $3, $1}' > ${TARGET_FILE} && echo "
-	File ${TARGET_FILE} generated ! "
+cat ${SOURCE_FILE} | sort | uniq | sed = | sed 'N;s|\n| = |' | awk '{print $3$2$1}' > ${TARGET_FILE} && echo "    File ${TARGET_FILE} successfully generated."
+
+
+# The formatting was very readable but not shell-friendly:
+#cat ${SOURCE_FILE} | sort | uniq | sed = | sed 'N;s|\n| = |' | awk '{printf "%-30s =  %s\n", $3, $1}' > ${TARGET_FILE} && echo "
+#	File ${TARGET_FILE} successfully generated."
 	
 	
