d7196ba362b0fff43ef59faa9d3dbc6d868d22ce
Olivier Boudeville <olivier.boudeville@esperide.com>
2008-03-28 10:06:46 +0000

    Gamma-correction added.

--- src/code/scripts/python/generate_master_palette.py
+++ src/code/scripts/python/generate_master_palette.py
@@ -2,6 +2,31 @@
 
 # See http://en.wikipedia.org/wiki/List_of_software_palettes#6-8-5_levels_RGB
 
+def quantize(component):
+	"""Converts a color component in [0;255] to [0.31] (5-bit)."""
+	return int( round(component/255.0*31) ) 
+	
+	
+# BGR, 5 bits per component, thus in [0,31].
+def convert_to_uint16(r,g,b):
+	# Base is 0xb1000000000000000 = 32768
+	base = 32768
+	base = (base | r) | (g<<5) | (b<<10)
+	return base
+		
+# See http://forum.gbadev.org/viewtopic.php?t=6484		
+ds_gamma_correction_factor = 2.3
+
+
+# Returns a 8-bit color component gamma-corrected for the DS.
+def gamma_correct(component):
+	# Let's suppose this is a proper way of dealing with gamma...
+	# See also for example ImageMagick-6.4.0magick/enhance.c:
+    #  gamma_map[i]=(MagickRealType) ScaleMapToQuantum((MagickRealType)
+    #    (MaxMap*pow((double) i/MaxMap,1.0/gamma)));
+	return int( round( ( (component/255.0)**(1/ds_gamma_correction_factor) ) * 255.0 ) )
+
+		
 r_range = 6
 g_range = 8
 b_range = 5
@@ -12,7 +37,10 @@ color_key = ( 106, 76, 48 )
 
 base_count= r_range*g_range*b_range
 
-print "Generating a master palette of %s base colors (for index in [1..%s]), with %s levels of red, %s of green and %s of blue. Index #0 is reserved for the colorkey, which is %s. The %s remaining index are used to add true grays and specific recurrent colors in target images." % ( base_count, base_count, r_range, g_range, b_range, color_key, 256 - base_count -1 )
+grey_range=8
+
+
+print "\nGenerating a master palette of %s base colors (for index in [1..%s]), with %s levels of red, %s of green and %s of blue. Index #0 is reserved for the colorkey, which is %s. The %s remaining index are used to add %s true grays and %s specific recurrent colors in target images.\n" % ( base_count, base_count, r_range, g_range, b_range, color_key, 256 - base_count -1, grey_range, 256 - base_count -1 - grey_range )
 
 
 base_palette = [ ( 
@@ -29,7 +57,6 @@ base_palette = [color_key] + base_palette
 # 15 index left.
 
 # Adding grey_range true greys:
-grey_range=8
 
 # Index offset chosen to remove pure black and pure white:  
 for grey in [ int( round(255*(g+1)/(grey_range+1)) ) for g in range(grey_range) ]:
@@ -64,10 +91,11 @@ for c in base_palette:
 print "Palette has %s color index." % (len(base_palette))
 
 
-palette_filename = "master-palette.rgb"
 
-print "Writing palette to '%s'." % (palette_filename)
-palette_file = open( palette_filename, 'w' )
+original_palette_filename = "master-palette-original.rgb"
+
+print " * Writing original master palette to '%s'." % (original_palette_filename)
+palette_file = open( original_palette_filename, 'w' )
 
 for c in base_palette:
 	palette_file.write( "%c" % (c[0]) )
@@ -76,5 +104,58 @@ for c in base_palette:
 
 palette_file.close()
 
-print "File written."
+gamma_corrected_palette_filename = "master-palette-gamma-corrected.rgb"
+
+print " * Writing gamma-corrected master palette to '%s'." % (gamma_corrected_palette_filename)
+palette_file = open( gamma_corrected_palette_filename, 'w' )
+
+for c in base_palette:
+	palette_file.write( "%c" % ( gamma_correct( c[0]) ) )
+	palette_file.write( "%c" % ( gamma_correct( c[1]) ) )
+	palette_file.write( "%c" % ( gamma_correct( c[2]) ) )
+
+palette_file.close()
+
+
+
+
+quantized_palette_filename = "master-palette-quantized.rgb"
+
+print " * Writing quantized master palette (non gamma-corrected) to '%s'." % (quantized_palette_filename)
+palette_file = open( quantized_palette_filename, 'w' )
+
+count=0
+for c in base_palette:
+	#print "  + quantized color index #%s: (%s, %s, %s)" % ( count,
+	#	quantize(c[0]), quantize(c[1]), quantize(c[2]) )
+	palette_file.write( "%c" % ( int( 255.0 /31 * quantize(c[0]) ) ) )
+	palette_file.write( "%c" % ( int( 255.0 /31 * quantize(c[1]) ) ) )
+	palette_file.write( "%c" % ( int( 255.0 /31 * quantize(c[2]) ) ) )
+	count +=1
+
+palette_file.close()
+
+
+
+final_palette_filename = "master-palette.pal"
+
+print " * Writing final master palette (gamma-corrected, then quantized, then encoded for the DS) to '%s'." % (final_palette_filename)
+palette_file = open( final_palette_filename, 'w' )
+
+for c in base_palette:
+	converted_color = convert_to_uint16( 
+		quantize( gamma_correct( c[0]) ),
+		quantize( gamma_correct( c[1]) ),
+		quantize( gamma_correct( c[2]) ) )
+	palette_file.write( "%c" % ( ( converted_color & 0xFF00 ) >> 8 ) )
+	palette_file.write( "%c" % (   converted_color & 0x00FF ) )
+
+palette_file.close()
+
+
+
+print "Master palette successfully generated."
+
+print "Use '%s' for color-reduction of assets, and '%s' as the actual DS palette." % ( quantized_palette_filename, final_palette_filename)
+
 
