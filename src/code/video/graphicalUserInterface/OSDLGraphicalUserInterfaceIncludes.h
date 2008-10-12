9fe99f7213e1a992785366c85e9aa6ea708c86b6
Olivier Boudeville <olivier.boudeville@esperide.com>
2008-10-12 18:53:14 +0000

    Added with Agar.

---
+++ src/code/video/graphicalUserInterface/OSDLGraphicalUserInterfaceIncludes.h
@@ -0,0 +1,13 @@
+#ifndef OSDL_GRAPHICAL_USER_INTERFACE_INCLUDES_H_
+#define OSDL_GRAPHICAL_USER_INTERFACE_INCLUDES_H_
+
+
+/*
+ * This include repository keeps track of headers of the module dedicated
+ * to graphical user interface rendering.
+ *
+ */
+ 
+ 
+#endif // OSDL_GRAPHICAL_USER_INTERFACE_INCLUDES_H_
+
