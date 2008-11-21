d0b75ce351382f3ef873b5e4ae1d388ebaab35d2
Olivier Boudeville <olivier.boudeville@esperide.com>
2008-11-21 11:46:38 +0000

    Updated, with reads from embedded files as well.

--- test/video/twoDimensional/testOSDLImage.cc
+++ test/video/twoDimensional/testOSDLImage.cc
@@ -4,20 +4,22 @@ using namespace OSDL::Video ;
 using namespace OSDL::Video::TwoDimensional ;
 
 using namespace Ceylan::Log ;
+using namespace Ceylan::System ;
 
 
 #include <string>
+using std::string ;
 
 
 
-/// Images location :
+/// Images location:
 const std::string firstImageFile  = "Soldier-heavy-purple-small.png" ;
 const std::string secondImageFile = "Battle-three-actors-small.jpg"  ;
 
 
 /*
  * Image font directory is defined relatively to OSDL documentation 
- * tree, from executable build directory :
+ * tree, from executable build directory:
  *
  */
 const std::string imageDirFromExec = "../../../src/doc/web/images" ;
@@ -25,14 +27,14 @@ const std::string imageDirFromExec = "../../../src/doc/web/images" ;
 
 /*
  * Image font directory is defined relatively to OSDL documentation tree,
- * from playTests.sh location in build tree :
+ * from playTests.sh location in build tree:
  *
  */
 const std::string imageDirForBuildPlayTests = "../src/doc/web/images" ;
 
 /*
  * Image font directory is defined relatively to OSDL documentation tree,
- * from playTests.sh location in installed tree :
+ * from playTests.sh location in installed tree:
  *
  */
 const std::string imageDirForInstalledPlayTests = "../OSDL/doc/web/images" ;
@@ -95,7 +97,7 @@ int main( int argc, char * argv[] )
 			
 			if ( token == "--online" )
 			{
-				// Ignored :
+				// Ignored:
 				tokenEaten = true ;
 			}
 			
@@ -109,12 +111,14 @@ int main( int argc, char * argv[] )
 			if ( ! tokenEaten )
 			{
 				throw Ceylan::CommandLineParseException( 
-					"Unexpected command line argument : " + token ) ;
+					"Unexpected command line argument: " + token ) ;
 			}
 		
 		}
 		
 		 
+		// Initialization section.
+
 		OSDL::CommonModule & myOSDL = OSDL::getCommonModule( 
 			CommonModule::UseVideo | CommonModule::UseKeyboard ) ;		
 		
@@ -130,9 +134,11 @@ int main( int argc, char * argv[] )
 		Surface & screen = myVideo.getScreenSurface() ;
 
 
+
+		// Section for standard operation, using standard files.
+
 		LogPlug::info( "Loading image located in " + firstImageFile ) ;
 				
-		screen.lock() ;
 
 		Ceylan::System::FileLocator imageFinder ;
 		
@@ -140,13 +146,16 @@ int main( int argc, char * argv[] )
 		imageFinder.addPath( imageDirForBuildPlayTests ) ;
 		imageFinder.addPath( imageDirForInstalledPlayTests ) ;
 		
+		screen.lock() ;
+		
+		screen.fill( Pixels::Yellow ) ;
 		screen.drawGrid() ;
+		
+		screen.unlock() ;
 
  		screen.loadImage( imageFinder.find( firstImageFile ), 
 			/* blit only */ true ) ;
 				
-		screen.unlock() ;
-
 		screen.update() ;
 		
 		if ( ! isBatch )				
@@ -155,17 +164,12 @@ int main( int argc, char * argv[] )
 			
 		LogPlug::info( "Loading image located in "	+ secondImageFile 
 			+ " with request of image format auto-detection." ) ;
-				
-		screen.lock() ;
 
-
- 		Surface & other = Surface::LoadImage( 
+		Surface & other = Surface::LoadImage( 
 			imageFinder.find( secondImageFile ) ) ;
 			
 		other.blitTo( screen, 270, 155 ) ;
 				
-		screen.unlock() ;
-
 		screen.update() ;
 		
 		if ( screenshotWanted )
@@ -175,7 +179,107 @@ int main( int argc, char * argv[] )
 			myOSDL.getEventsModule().waitForAnyKey() ;
 			
 		delete & other ;
+			
+			
+			
+			
+		// Section for image loading from archive-embedded file.
 					
+					
+					
+		LogPlug::info( 
+        	"Now, trying to read images from an archive-embedded file." ) ;
+
+		EmbeddedFileSystemManager::ArchiveFileLocator.addPath( "../../basic" ) ;
+		
+		const string archiveFilename = 
+			"test-OSDLEmbeddedFileSystem-archive.oar" ;
+		     
+	 	string archiveFullPath ;
+		
+		try
+		{
+			
+			// This is (implicitly) the standard filesystem manager here:
+			archiveFullPath = EmbeddedFileSystemManager::FindArchivePath( 
+				archiveFilename ) ;
+				
+		}
+		catch( const EmbeddedFileSystemManagerException & e )
+		{
+		        
+        	LogPlug::warning( "Test archive '" + archiveFilename 
+            	+ "' not found (" + e.toString() + "), run the "
+                "create-testOSDLEmbeddedFileSystem-archive.sh script "
+                "beforehand to have it ready for this test. Stopping now." ) ;
+                
+        	return 0 ;
+            
+        }
+
+		LogPlug::info( "Test archive '" + archiveFilename 
+        	+ "' found, mounting it." ) ;
+			
+		// Keep the standard manager, to restore it:
+		FileSystemManager & standardFSManager =
+        	FileSystemManager::GetExistingDefaultFileSystemManager() ;
+        
+
+        EmbeddedFileSystemManager & myEmbedddedManager = 		
+        	EmbeddedFileSystemManager::GetEmbeddedFileSystemManager() ;
+        
+ 		myEmbedddedManager.chooseBasicSettings( /* organization name */ "OSDL",
+            /* application name */ "testOSDLImage" ) ;
+
+		myEmbedddedManager.setWriteDirectory( "." ) ;
+        
+ 		myEmbedddedManager.mount( archiveFullPath ) ;
+
+        // Thus images will be searched in specified archive:    
+        FileSystemManager::SetDefaultFileSystemManager( myEmbedddedManager,
+        	/* deallocatePreviousIfAny */ false ) ;
+			
+        string targetEmbeddedPNG = "Soldier-heavy-purple-small.png" ;
+
+		// Preload implied; platform-independent paths:
+ 		Surface & embeddedImage = Surface::LoadImage( 
+			"test-OSDLEmbeddedFileSystem-archive/" + targetEmbeddedPNG  ) ;
+				
+		screen.lock() ;
+		
+		screen.fill( Pixels::Red ) ;
+		
+		screen.unlock() ;
+
+		Surface & flippedImage = embeddedImage.flipVertical() ;
+
+		delete & embeddedImage ;
+		
+		flippedImage.blitTo( screen, 80, 50 ) ;
+
+		delete & flippedImage ;
+
+        string targetEmbeddedJPG = "Rune-stone-small.jpg" ;
+		
+		// Preload implied; platform-independent paths:
+ 		Surface & otherEmbeddedImage = Surface::LoadJPG( 
+			"test-OSDLEmbeddedFileSystem-archive/" 	+ targetEmbeddedJPG,  
+			/* blitOnly */ false ) ;
+
+		otherEmbeddedImage.blitTo( screen, 300, 50 ) ;
+		
+		delete & otherEmbeddedImage ;
+		
+		screen.update() ;
+		
+		if ( ! isBatch )				
+			myOSDL.getEventsModule().waitForAnyKey() ;
+					
+ 		myEmbedddedManager.umount( archiveFullPath ) ;
+
+		// Will deallocate embedded FS manager; prepare for log writing:
+		FileSystemManager::SetDefaultFileSystemManager( standardFSManager ) ;
+
 		LogPlug::info( "Stopping OSDL." ) ;		
         OSDL::stop() ;
 		
@@ -187,7 +291,7 @@ int main( int argc, char * argv[] )
     catch ( const OSDL::Exception & e )
     {
 	
-        LogPlug::error( "OSDL exception caught : "
+        LogPlug::error( "OSDL exception caught: "
         	 + e.toString( Ceylan::high ) ) ;
        	return Ceylan::ExitFailure ;
 
@@ -196,7 +300,7 @@ int main( int argc, char * argv[] )
     catch ( const Ceylan::Exception & e )
     {
 	
-        LogPlug::error( "Ceylan exception caught : "
+        LogPlug::error( "Ceylan exception caught: "
         	 + e.toString( Ceylan::high ) ) ;
        	return Ceylan::ExitFailure ;
 
@@ -205,7 +309,7 @@ int main( int argc, char * argv[] )
     catch ( const std::exception & e )
     {
 	
-        LogPlug::error( "Standard exception caught : " 
+        LogPlug::error( "Standard exception caught: " 
 			 + std::string( e.what() ) ) ;
        	return Ceylan::ExitFailure ;
 
