5fd1c51d04d041210a78fb9fd2f1c3333894bad5
Olivier Boudeville <olivier.boudeville@esperide.com>
2008-12-05 10:57:24 +0000

    Slight reformatting.

--- src/conf/build/cleanGeneratedConfigFiles.sh
+++ src/conf/build/cleanGeneratedConfigFiles.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-USAGE="Usage : "`basename $0`" : cleans the install by removing all files generated for the build system. Useful while debugging the autotools."
+USAGE="Usage: "`basename $0`": cleans the install by removing all files generated for the build system. Useful while debugging the autotools."
 
 FIND=`which find 2>/dev/null`
 #echo "FIND = $FIND"
@@ -9,7 +9,7 @@ FIND=`which find 2>/dev/null`
 RM="/bin/rm"
 
 # Must act from very first root directory.
-# Ex : from OSDL/trunk, not OSDL/trunk/src
+# Ex: from OSDL/trunk, not OSDL/trunk/src
 BASE_DIR=`dirname $0`/../../..
 #echo "BASE_DIR = $BASE_DIR"
 
@@ -18,7 +18,7 @@ cd $BASE_DIR
 
  
  
-# Only the Makefile (if any) knows which executables should be removed :
+# Only the Makefile (if any) knows which executables should be removed:
 cd test
 #echo "cleaning test"
 make clean 1>/dev/null 2>&1
@@ -31,10 +31,10 @@ make clean 1>/dev/null 2>&1
 cd ..
 
 
-# Directories :
+# Directories:
 $FIND . \( -name 'autom4te.cache' -o -name '.deps' -o -name '.libs' -o -name 'test*.exe-logs' \) -exec $RM -rf '{}' ';' 2>/dev/null
 
-# Files :
+# Files:
 $FIND . \( -name ltmain.sh -o -name aclocal.m4 -o -name install-sh -o -name missing -o -name depcomp -o -name stamp-h1 -o -name configure.ac -o -name configure-from-autoscan.ac -o -name configure-from-autoupdate.ac -o -name configure -o -name 'auto*.log' -o -name 'auto*.err' -o -name config.guess -o -name config.log -o -name config.status -o -name config.sub -o -name libtool -o -name Makefile -o -name Makefile.in -o -name OSDLTemporaryConfig.h -o -name OSDLConfig.h -o -name OSDLHeaderVersion.cc -o -name '*.so' -o -name '*.a' -o -name 'svn-commit*.tmp' -o -name '*.o' -o -name '*.loT' -o -name '*.lo' -o -name 'test*.log' -o -name 'test*.png' -o -name 'test*.bmp' -o -name '*.la' -o -name core \) -exec $RM -f '{}' ';' 2>/dev/null
 
 $RM -f TODO MAINTENERS AUTHORS INSTALL FAQ ChangeLog THANKS NEWS COPYING.LIB README LOANI-README src/conf/build/loani/LOANI-README-template.txt src/conf/build/loani/LOANI-README osdl-*.tar.gz osdl-*.tar.bz2 test/testsOutcome.txt
