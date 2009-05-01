557b6795b42c0eee6e17e560ee10db9ce29b2625
Olivier Boudeville <olivier.boudeville@esperide.com>
2009-05-01 17:33:19 +0000

    Standard header added.

--- src/code/basic/OSDLARM7Base.c
+++ src/code/basic/OSDLARM7Base.c
@@ -1,3 +1,29 @@
+/* 
+ * Copyright (C) 2003-2009 Olivier Boudeville
+ *
+ * This file is part of the OSDL library.
+ *
+ * The OSDL library is free software: you can redistribute it and/or modify
+ * it under the terms of either the GNU Lesser General Public License or
+ * the GNU General Public License, as they are published by the Free Software
+ * Foundation, either version 3 of these Licenses, or (at your option) 
+ * any later version.
+ *
+ * The OSDL library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License and the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License and of the GNU General Public License along with the OSDL library.
+ * If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Author: Olivier Boudeville (olivier.boudeville@esperide.com)
+ *
+ */
+
+
 #pragma GCC diagnostic ignored "-Wsign-compare"
 
 /* To have Ceylan defines so that Ceylan C headers can be directly included: */
@@ -544,6 +570,7 @@ bool decodeNextMP3Frame()
 
 
 
+
 /* Section dedicated to library integration of OSDL IPC. */
 
 
@@ -697,7 +724,7 @@ void handleFadeInMusicRequest( FIFOElement firstElement )
 		
 	SampleRate usedRate ;
 	
-	/* A mp3 frame already decoded ? If yes, sample rate is known: */
+	/* A mp3 frame already decoded? If yes, sample rate is known: */
 	if ( outputSampleRate == 0 )
 	{
 		
@@ -745,7 +772,7 @@ void handleFadeOutMusicRequest( FIFOElement firstElement )
 		
 	SampleRate usedRate ;
 	
-	/* An mp3 frame already decoded ? */
+	/* An mp3 frame already decoded? */
 	if ( outputSampleRate == 0 )
 	{
 		
