ce7ea8983012e652b2645c733b7cbdd6c16ed41f
Olivier Boudeville <olivier.boudeville@esperide.com>
2010-02-06 19:09:06 +0000

    Better SDL_gfx support. Reformatted.

--- test/autogen.sh
+++ test/autogen.sh
@@ -10,7 +10,7 @@ Usage: "`basename $0`" [ --with-osdl-env-file <filename> ] [ -g | --guess-osdl-e
 	--only-prepare-dist: perform only necessary operations so that the test directory can be distributed afterwards"
 
 # These tests must rely on a related Ceylan source directory, since they:
-#	- need to know which Ceylan version is to be tested
+#	- need to know which Ceylan version is t1o be tested
 #	- use some Ceylan facilities (ex: Ceylan substitute script)
 
 
@@ -32,7 +32,7 @@ debug()
 {
 	if [ $debug_mode -eq 0 ] ; then
 		echo "debug: $*"
-	fi	
+	fi
 }
 
 
@@ -51,8 +51,8 @@ wait()
 	if [ $wait_activated -eq 0 ] ; then
 		echo "  <press enter key to continue>"
 		read
-	fi	
-	
+	fi
+
 }
 
 
@@ -77,24 +77,24 @@ osdl_environment_file=""
 
 while [ $# -gt 0 ] ; do
 	token_eaten=1
-		
-		
+
+
 	if [ "$1" = "-g" -o "$1" = "--guess-osdl-environment" ] ; then
-	
+
 		osdl_environment_file=${loani_installations}/OSDL-environment.sh
-		
+
 		if [ ! -f "${osdl_environment_file}" ] ; then
 			echo "Error, guessed OSDL environment file not found (${osdl_environment_file}), please specify it.
-$USAGE" 1>&2		
+$USAGE" 1>&2
 			exit 6
 		fi
-			
-		warning "Guessed OSDL environment file is ${osdl_environment_file}." 
+
+		warning "Guessed OSDL environment file is ${osdl_environment_file}."
 		. ${osdl_environment_file}
 		token_eaten=0
-		
+
 	fi
-	
+
 
 	if [ "$1" = "-n" -o "$1" = "--no-build" ] ; then
 		do_stop_after_configure=0
@@ -105,19 +105,19 @@ $USAGE" 1>&2
 		do_stop_after_configure=0
 		token_eaten=0
 	fi
-	
+
 	if [ "$1" = "--with-osdl-env-file" ] ; then
 		shift
 		osdl_environment_file="$1"
 		if [ ! -f "${osdl_environment_file}" ] ; then
 			echo "Error, specified OSDL environment file does not exist ($osdl_environment_file).
-$USAGE" 1>&2	
+$USAGE" 1>&2
 			exit 10
 		fi
 		. ${osdl_environment_file}
 		token_eaten=0
 	fi
-	
+
 	if [ "$1" = "-h" -o "$1" = "--help" ] ; then
 		echo "$USAGE"
 		exit
@@ -127,28 +127,34 @@ $USAGE" 1>&2
 	if [ $token_eaten -eq 1 ] ; then
 		echo "Error, unknown argument ($1).$USAGE" 1>&2
 		exit 4
-	fi	
+	fi
 	shift
 done
 
 
 
-
+sdl_gfx_support_activated=false
 
 # Where the libraries should be found:
 if [ -n "$osdl_environment_file" ] ; then
 
 	# If these lines are changed, change accordingly loani-requiredTools.sh,
 	# search for "Making tests for OSDL".
-	
-	osdl_install_prefix_opt="--with-osdl-prefix=$OSDL_PREFIX"	
-	ceylan_install_prefix_opt="--with-ceylan-prefix=$Ceylan_PREFIX"	
+
+	osdl_install_prefix_opt="--with-osdl-prefix=$OSDL_PREFIX"
+	ceylan_install_prefix_opt="--with-ceylan-prefix=$Ceylan_PREFIX"
 	sdl_install_prefix_opt="--with-sdl-prefix=$SDL_PREFIX"
 	libjpeg_install_prefix_opt="--with-libjpeg-prefix=$libjpeg_PREFIX"
 	zlib_install_prefix_opt="--with-zlib-prefix=$zlib_PREFIX"
 	libpng_install_prefix_opt="--with-libpng-prefix=$libpng_PREFIX"
 	sdl_image_install_prefix_opt="--with-sdl_image-prefix=$SDL_image_PREFIX"
-	sdl_gfx_install_prefix_opt="--with-sdl_gfx-prefix=$SDL_gfx_PREFIX"
+
+	if [ "$sdl_gfx_support_activated" = "true" ] ; then
+		sdl_gfx_install_prefix_opt="--with-sdl_gfx-prefix=$SDL_gfx_PREFIX"
+	else
+		sdl_gfx_install_prefix_opt="--with-sdl_gfx-prefix=no"
+	fi
+
 	freetype_install_prefix_opt="--with-freetype-prefix=$freetype_PREFIX"
 	sdl_ttf_install_prefix_opt="--with-sdl_ttf-prefix=$SDL_ttf_PREFIX"
 	libogg_install_prefix_opt="--with-ogg=$libogg_PREFIX"
@@ -156,9 +162,9 @@ if [ -n "$osdl_environment_file" ] ; then
 	sdl_mixer_install_prefix_opt="--with-sdl_mixer-prefix=$SDL_mixer_PREFIX"
 	libagar_install_prefix_opt="--with-libagar-prefix=$Agar_PREFIX"
 	physicsfs_install_prefix_opt="--with-physicsfs-prefix=$PhysicsFS_PREFIX"
-	
+
 	prerequesites_prefix_opt="$osdl_install_prefix_opt $ceylan_install_prefix_opt $sdl_install_prefix_opt $libjpeg_install_prefix_opt $zlib_install_prefix_opt $libpng_install_prefix_opt $sdl_image_install_prefix_opt $sdl_gfx_install_prefix_opt $freetype_install_prefix_opt $sdl_ttf_install_prefix_opt $libogg_install_prefix_opt $libvorbis_install_prefix_opt $sdl_mixer_install_prefix_opt $libagar_install_prefix_opt $physicsfs_install_prefix_opt"
-	
+
 fi
 
 
@@ -185,13 +191,13 @@ else
 	# Default prefix:
 	ceylan_location="/usr/local"
 fi
-		
+
 if [ ! -d "$ceylan_location" ] ; then
-	
+
 	echo "Error, Ceylan prefix retrieved thanks to guessed OSDL environment file ($osdl_environment_file) does not exist ($ceylan_location)" 1>&2
 	exit 7
-		
-fi	
+
+fi
 
 
 if [ -n "$OSDL_PREFIX" ] ; then
@@ -201,13 +207,13 @@ else
 	osdl_location="/usr/local"
 fi
 
-	
+
 if [ ! -d "$osdl_location" ] ; then
-	
+
 	echo "Error, OSDL prefix retrieved thanks to guessed OSDL environment file ($osdl_environment_file) does not exist ($osdl_location)" 1>&2
 	exit 8
-		
-fi	
+
+fi
 
 
 # Searches for the Ceylan substitute script:
@@ -255,7 +261,7 @@ copy="--copy"
 force="--force"
 #force=""
 
-# Warning selection: 
+# Warning selection:
 warnings="--warnings=all"
 #warnings=""
 
@@ -271,7 +277,7 @@ execute()
 {
 
 	echo "    Executing $*"
-	
+
 	if [ $log_on_file -eq 0 ] ; then
 		echo "    Executing $* from "`pwd` >>"$log_filename"
 		eval $* >>"$log_filename" 2>&1
@@ -279,7 +285,7 @@ execute()
 		echo "----------------------------------------" >>"$log_filename"
 		echo >>"$log_filename"
 	else
-		eval $* 
+		eval $*
 		RES=$?
 	fi
 
@@ -291,38 +297,38 @@ execute()
 			echo "Error while executing '$*'" 1>&2
 			if [ "$1" = "./configure" ]; then
 				echo "
-Note: check test/config.log"	
-  			fi
+Note: check test/config.log"
+			fi
 		fi
-			
+
 		exit $RES
 	fi
-	
+
 	wait
-	
-}                 
-    
-	                                                 
+
+}
+
+
 generateCustom()
-# Old-fashioned way of regenerating the build system from scratch: 
+# Old-fashioned way of regenerating the build system from scratch:
 {
 
 
 	echo "--- generating build system"
-	
+
 	if [ $do_remove_generated -eq 0 ] ; then
 		echo
 		echo " - removing all generated files"
 		./cleanGeneratedConfigFiles.sh
 	fi
-	
+
 	# Update timestamps since SVN may mess them up:
 	CONFIG_SOURCE=configure-template.ac
 	CONFIG_TARGET=configure.ac
-	
+
 	touch $CONFIG_SOURCE
-	
-		
+
+
 	echo
 	echo " - generating $CONFIG_TARGET, by filling $CONFIG_SOURCE with ${OSDL_SETTINGS_FILE}"
 
@@ -331,34 +337,34 @@ generateCustom()
 
 	echo
 	echo " - preparing libtool, by executing libtoolize"
-	
-	
+
+
 	(libtool --version) < /dev/null > /dev/null 2>&1 || {
 		echo
 		echo "**Error**: You must have 'libtool' installed."
-		echo "You can get it from: ftp://ftp.gnu.org/pub/gnu/" 
+		echo "You can get it from: ftp://ftp.gnu.org/pub/gnu/"
 		exit 20
-   	}
-	
+	}
+
 	(libtoolize --version) < /dev/null > /dev/null 2>&1 || {
 		echo
 		echo "**Error**: You must have 'libtoolize' installed."
-		echo "You can get it from: ftp://ftp.gnu.org/pub/gnu/" 
+		echo "You can get it from: ftp://ftp.gnu.org/pub/gnu/"
 		exit 21
-   	}
+	}
 
 	if test -z "$verbose"; then
 		libtoolize_verbose=""
 	else
 		libtoolize_verbose="--debug"
 	fi
-	
+
 	# No --ltdl here to avoid the copy of useless directories in tests:
 	execute libtoolize --install --automake $copy $force $libtoolize_verbose
-	
+
 	echo
 	echo " - generating aclocal.m4, by scanning configure.ac"
-	
+
 	(aclocal --version) < /dev/null > /dev/null 2>&1 || {
 		echo
 		echo "**Error**: Missing 'aclocal'.  The version of 'automake'"
@@ -367,14 +373,14 @@ generateCustom()
 		exit 22
 	}
 
-	# Where ceylan.m4, pkg.m4, etc. should be found: 
+	# Where ceylan.m4, pkg.m4, etc. should be found:
 	CEYLAN_M4_DIR=$ceylan_location/share/Ceylan
-	
-	# Where osdl.m4, sdl.m4, etc. should be found: 
+
+	# Where osdl.m4, sdl.m4, etc. should be found:
 	OSDL_M4_DIR=$osdl_location/share/OSDL
-	
+
 	ACLOCAL_OUTPUT=aclocal.m4
-	
+
 	# With newer libtool (ex: 2.2.4), we need to include a whole bunch of *.m4
 	# files, otherwise 'warning: LTOPTIONS_VERSION is m4_require'd but not
 	# m4_defun'd' ... ', same thing for LTSUGAR_VERSION, LTVERSION_VERSION, etc.
@@ -382,7 +388,7 @@ generateCustom()
 
 	# Do not use '--acdir=.' since it prevents aclocal from writing its file:
 	execute aclocal -I ${GUESSED_LIBTOOL_BASE}/share/aclocal -I $CEYLAN_M4_DIR -I $OSDL_M4_DIR --output=$ACLOCAL_OUTPUT $force $verbose
-	
+
 	echo
 	echo " - generating '.in' files from '.am' files with automake"
 
@@ -393,12 +399,12 @@ generateCustom()
 		exit 24
 	}
 
-	automake_strictness="--foreign" 
+	automake_strictness="--foreign"
 	#automake_strictness="--gnu"
-	
-	execute automake --add-missing --include-deps $automake_strictness $warnings $copy $verbose  
 
-	
+	execute automake --add-missing --include-deps $automake_strictness $warnings $copy $verbose
+
+
 	(autoconf --version) < /dev/null > /dev/null 2>&1 || {
 		echo
 		echo "**Error**: You must have 'autoconf' installed."
@@ -406,72 +412,72 @@ generateCustom()
 		echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/"
 		exit 25
 	}
-	
+
 	echo
 	echo " - generating 'configure' script"
- 	execute autoconf $warnings $force $verbose
+	execute autoconf $warnings $force $verbose
 
 	# Add GNU gettext (autopoint)?
-	
+
 	if [ $do_stop_after_configure -eq 0 ] ; then
 		echo
 		echo "Now you are ready to run configure"
 		return
 	fi
-		
+
 	echo
 	echo " - executing 'configure' script"
-	
+
 	(./configure --version) < /dev/null > /dev/null 2>&1 || {
 		echo
 		echo "**Error**: the 'configure' cannot be properly used"
 		exit 26
 	}
-		
- 	execute ./configure $configure_opt
-	
+
+	execute ./configure $configure_opt
+
 
 	if [ $do_clean -eq 0 ] ; then
 		echo
 		echo " - cleaning all"
-	 	execute make clean
+		execute make clean
 	fi
-	
-	
+
+
 	if [ $do_build -eq 0 ] ; then
 		echo
 		echo " - building all"
-	 	execute make
+		execute make
 	fi
-	
-	
+
+
 	if [ $do_install -eq 0 ] ; then
 		echo
 		echo " - installing"
-	 	execute make install
+		execute make install
 	else
-	
+
 		if [ -n "$osdl_install_prefix_opt" ] ; then
 			echo 1>&2
 			echo "Warning: not installing tests and using $osdl_install_prefix_opt implies updating library search paths to select the correct library, for example one may enter: " 1>&2
 			echo "export LD_LIBRARY_PATH=$osdl_install_prefix/lib:\$LD_LIBRARY_PATH" 1>&2
 		fi
-		
+
 	fi
 
 	if [ $do_test -eq 0 ] ; then
 		export LD_LIBRARY_PATH=$ceylan_install_prefix/lib:$LD_LIBRARY_PATH
 		echo
 		echo " - running unit tests"
-	 	execute make check
+		execute make check
 	fi
-	
-	
+
+
 	if [ -n "$osdl_environment_file" ] ; then
 		echo "Note that before running tests from the command-line, the runtime environment must be set (ex: OSDL-environment.sh must have been sourced beforehand)."
 	fi
-	 
-		
+
+
 }
 
 
@@ -481,15 +487,14 @@ regenerateWithAutoreconf()
 {
 
 	echo "--- updating build system using autoreconf"
-	
+
 	autoreconf_opt="--force --install"
-	autoreconf_warnings="--warnings=all" 
-	
+	autoreconf_warnings="--warnings=all"
+
 	autoreconf $autoreconf_opt $autoreconf_warnings
-	
+
 }
-	
-	
+
+
 generateCustom
 #regenerateWithAutoreconf
-
