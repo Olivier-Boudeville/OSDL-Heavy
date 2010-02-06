a979e47343ac5e17d8f62c408a312a78d0f088b7
Olivier Boudeville <olivier.boudeville@esperide.com>
2010-02-06 19:16:24 +0000

    Reformatted.

--- src/code/basic/OSDLEmbeddedDirectory.cc
+++ src/code/basic/OSDLEmbeddedDirectory.cc
@@ -1,4 +1,4 @@
-/* 
+/*
  * Copyright (C) 2003-2009 Olivier Boudeville
  *
  * This file is part of the OSDL library.
@@ -6,7 +6,7 @@
  * The OSDL library is free software: you can redistribute it and/or modify
  * it under the terms of either the GNU Lesser General Public License or
  * the GNU General Public License, as they are published by the Free Software
- * Foundation, either version 3 of these Licenses, or (at your option) 
+ * Foundation, either version 3 of these Licenses, or (at your option)
  * any later version.
  *
  * The OSDL library is distributed in the hope that it will be useful,
@@ -59,10 +59,10 @@
  * is expected to use the embedded filesystem manager, whereas the latter shall
  * use the default filesystem manager, which may or may not be the embedded
  * one.
- * 
+ *
  */
- 
- 
+
+
 using std::string ;
 using std::list ;
 
@@ -79,7 +79,7 @@ using namespace OSDL ;
 EmbeddedDirectory::~EmbeddedDirectory() throw()
 {
 
-	// Nothing special here for embedded directories.
+  // Nothing special here for embedded directories.
 
 }
 
@@ -90,10 +90,10 @@ EmbeddedDirectory::~EmbeddedDirectory() throw()
 
 
 // Instance methods.
-				
 
-			
-				
+
+
+
 // Directory content subsection.
 
 
@@ -103,31 +103,31 @@ bool EmbeddedDirectory::hasDirectory( const string & subdirectoryName ) const
 
 #if OSDL_USES_PHYSICSFS
 
-	FileSystemManager * fsManager ;
-	
-	try
-	{
-	
-		fsManager = & getCorrespondingFileSystemManager() ;
-		
-	}
-	catch( const DirectoryDelegatingException & e )
-	{
-	
-		throw DirectoryLookupFailed( 
-			"EmbeddedDirectory::hasDirectory failed: " + e.toString() ) ;
-			 
-	}	
-	
-	string tmpPath = fsManager->joinPath( _path, subdirectoryName ) ;
-	
-	return ( PHYSFS_isDirectory( tmpPath.c_str() ) != 0 ) ;
+  FileSystemManager * fsManager ;
+
+  try
+  {
+
+	fsManager = & getCorrespondingFileSystemManager() ;
+
+  }
+  catch( const DirectoryDelegatingException & e )
+  {
+
+	throw DirectoryLookupFailed(
+	  "EmbeddedDirectory::hasDirectory failed: " + e.toString() ) ;
+
+  }
+
+  string tmpPath = fsManager->joinPath( _path, subdirectoryName ) ;
+
+  return ( PHYSFS_isDirectory( tmpPath.c_str() ) != 0 ) ;
 
 #else // OSDL_USES_PHYSICSFS
 
-	throw DirectoryLookupFailed( "EmbeddedDirectory::hasDirectory failed: "
-    	"no PhysicsFS support available." ) ;  
-          	
+  throw DirectoryLookupFailed( "EmbeddedDirectory::hasDirectory failed: "
+	"no PhysicsFS support available." ) ;
+
 #endif // OSDL_USES_PHYSICSFS
 
 }
@@ -139,37 +139,37 @@ bool EmbeddedDirectory::hasFile( const string & fileName ) const
 
 #if OSDL_USES_PHYSICSFS
 
-	FileSystemManager * fsManager ;
-	
-	try
-	{
-	
-		fsManager = & getCorrespondingFileSystemManager() ;
-		
-	}
-	catch( const DirectoryDelegatingException & e )
-	{
-	
-		throw DirectoryLookupFailed( 
-			"EmbeddedDirectory::hasFile failed: " + e.toString() ) ;
-			 
-	}	
+  FileSystemManager * fsManager ;
 
-	// It is a file or a symlink if it exists and is not a directory:
-	
-	string tmpPath = fsManager->joinPath( _path, fileName ) ;
-	
-    if ( PHYSFS_exists( tmpPath.c_str() ) == 0 )
-    	return false ;
-    
-    // Here it exists, thus it must not be a directory to be a file:    
-	return ( PHYSFS_isDirectory( tmpPath.c_str() ) == 0 ) ;
+  try
+  {
+
+	fsManager = & getCorrespondingFileSystemManager() ;
+
+  }
+  catch( const DirectoryDelegatingException & e )
+  {
+
+	throw DirectoryLookupFailed(
+	  "EmbeddedDirectory::hasFile failed: " + e.toString() ) ;
+
+  }
+
+  // It is a file or a symlink if it exists and is not a directory:
+
+  string tmpPath = fsManager->joinPath( _path, fileName ) ;
+
+  if ( PHYSFS_exists( tmpPath.c_str() ) == 0 )
+	return false ;
+
+  // Here it exists, thus it must not be a directory to be a file:
+  return ( PHYSFS_isDirectory( tmpPath.c_str() ) == 0 ) ;
 
 #else // OSDL_USES_PHYSICSFS
 
-	throw DirectoryLookupFailed( "EmbeddedDirectory::hasFile failed: "
-    	"no PhysicsFS support available." ) ;  
-          	
+  throw DirectoryLookupFailed( "EmbeddedDirectory::hasFile failed: "
+	"no PhysicsFS support available." ) ;
+
 #endif // OSDL_USES_PHYSICSFS
 
 }
@@ -181,99 +181,99 @@ bool EmbeddedDirectory::hasEntry( const string & entryName ) const
 
 #if OSDL_USES_PHYSICSFS
 
-	FileSystemManager * fsManager ;
-	
-	try
-	{
-	
-		fsManager = & getCorrespondingFileSystemManager() ;
-		
-	}
-	catch( const DirectoryDelegatingException & e )
-	{
-	
-		throw DirectoryLookupFailed( 
-			"EmbeddedDirectory::hasEntry failed: " + e.toString() ) ;
-			 
-	}	
-	
-	string tmpPath = fsManager->joinPath( _path, entryName ) ;
-	
-	return ( PHYSFS_exists( tmpPath.c_str() ) != 0 ) ;
+  FileSystemManager * fsManager ;
+
+  try
+  {
+
+	fsManager = & getCorrespondingFileSystemManager() ;
+
+  }
+  catch( const DirectoryDelegatingException & e )
+  {
+
+	throw DirectoryLookupFailed(
+	  "EmbeddedDirectory::hasEntry failed: " + e.toString() ) ;
+
+  }
+
+  string tmpPath = fsManager->joinPath( _path, entryName ) ;
+
+  return ( PHYSFS_exists( tmpPath.c_str() ) != 0 ) ;
 
 #else // OSDL_USES_PHYSICSFS
 
-	throw DirectoryLookupFailed( "EmbeddedDirectory::hasEntry failed: "
-    	"no PhysicsFS support available." ) ;  
-          	
+  throw DirectoryLookupFailed( "EmbeddedDirectory::hasEntry failed: "
+	"no PhysicsFS support available." ) ;
+
 #endif // OSDL_USES_PHYSICSFS
 
 }
 
 
-			
-					
+
+
 void EmbeddedDirectory::getSubdirectories( list<string> & subDirectories ) const
 {
 
-	/*
-     * Could be implemented if PHYSFS_enumerateFiles returned directory
-     * (to check)
-     *
-     */
-    throw DirectoryLookupFailed( " EmbeddedDirectory::getSubdirectories: "
-    	"no PhysicsFS support available." ) ;  
-    	
+  /*
+   * Could be implemented if PHYSFS_enumerateFiles returned directory
+   * (to check)
+   *
+   */
+  throw DirectoryLookupFailed( " EmbeddedDirectory::getSubdirectories: "
+	"no PhysicsFS support available." ) ;
+
 }
 
-					
-										
+
+
 void EmbeddedDirectory::getFiles( list<string> & files ) const
 {
 
 #if OSDL_USES_PHYSICSFS
 
-	char **rc = PHYSFS_enumerateFiles( _path.c_str() ) ;
-    
-	char **i ;
-	
-    for ( i = rc; *i != 0; i++ )
-    	files.push_back( *i ) ;
+  char **rc = PHYSFS_enumerateFiles( _path.c_str() ) ;
 
-	PHYSFS_freeList( rc ) ;		
-    
+  char **i ;
+
+  for ( i = rc; *i != 0; i++ )
+	files.push_back( *i ) ;
+
+  PHYSFS_freeList( rc ) ;
+
 #else // OSDL_USES_PHYSICSFS
 
-	throw Ceylan::System::DirectoryLookupFailed( 
-    	"EmbeddedDirectory::getFiles failed: "
-    	"no PhysicsFS support available." ) ;
-        
+  throw Ceylan::System::DirectoryLookupFailed(
+	"EmbeddedDirectory::getFiles failed: "
+	"no PhysicsFS support available." ) ;
+
 #endif // OSDL_USES_PHYSICSFS
 
 }
 
-	
-					
+
+
 void EmbeddedDirectory::getEntries( list<string> & entries ) const
 {
 
 #if OSDL_USES_PHYSICSFS
 
-	char **rc = PHYSFS_enumerateFiles( _path.c_str() ) ;
-    
-	char **i ;
-	
-    for ( i = rc; *i != 0; i++ )
-    	entries.push_back( *i ) ;
+  char **rc = PHYSFS_enumerateFiles( _path.c_str() ) ;
 
-	PHYSFS_freeList( rc ) ;		
-    
+  char **i ;
+
+  for ( i = rc; *i != 0; i++ )
+	entries.push_back( *i ) ;
+
+  PHYSFS_freeList( rc ) ;
+
 #else // OSDL_USES_PHYSICSFS
 
-	throw Ceylan::System::DirectoryLookupFailed( 
-    	"EmbeddedDirectory::getEntries failed: "
-    	"no PhysicsFS support available." ) ;
-        
+  throw Ceylan::System::DirectoryLookupFailed(
+	"EmbeddedDirectory::getEntries failed: "
+	"no PhysicsFS support available." ) ;
+
 #endif // OSDL_USES_PHYSICSFS
 
 }
@@ -281,93 +281,93 @@ void EmbeddedDirectory::getEntries( list<string> & entries ) const
 
 
 void EmbeddedDirectory::getSortedEntries( list<string> & subDirectories,
-	list<string> & files, list<string> & otherEntries ) const
+  list<string> & files, list<string> & otherEntries ) const
 {
 
-	/*
-     * Could be implemented if PHYSFS_enumerateFiles returned directory
-     * (to check)
-     *
-     */
-    throw DirectoryLookupFailed( "EmbeddedDirectory::getSortedEntries: "
-    	"no PhysicsFS support available." ) ;  
+  /*
+   * Could be implemented if PHYSFS_enumerateFiles returned directory
+   * (to check)
+   *
+   */
+  throw DirectoryLookupFailed( "EmbeddedDirectory::getSortedEntries: "
+	"no PhysicsFS support available." ) ;
 
 }
-	
-	
-	
-					
+
+
+
+
 // Other instance methods.
 
-										
-					
+
+
 const std::string EmbeddedDirectory::toString( Ceylan::VerbosityLevels level )
-	const
+  const
 {
 
-	return "Embedded directory referring to user-specified path '" 
-		+ Ceylan::encodeToROT13( _path ) 
-		+ "', corresponding to an actual path in archive '" + _path + "'" ;
- 
+  return "Embedded directory referring to user-specified path '"
+	+ Ceylan::encodeToROT13( _path )
+	+ "', corresponding to an actual path in archive '" + _path + "'" ;
+
 }
-					
-		
-		
-					
-					
-// Factory section.										
+
+
+
+
+
+// Factory section.
 
 
 EmbeddedDirectory & EmbeddedDirectory::Create( const string & newDirectoryName )
 {
 
-	return * new EmbeddedDirectory( newDirectoryName, /* create */ true ) ;
+  return * new EmbeddedDirectory( newDirectoryName, /* create */ true ) ;
 
 }
 
 
-					
-EmbeddedDirectory & EmbeddedDirectory::Open( const string & directoryName ) 
+
+EmbeddedDirectory & EmbeddedDirectory::Open( const string & directoryName )
 {
 
-	return * new EmbeddedDirectory( directoryName, /* create */ false ) ;
+  return * new EmbeddedDirectory( directoryName, /* create */ false ) ;
 
 }
-			
 
-			
-EmbeddedDirectory::EmbeddedDirectory( const string & directoryName, 
-		bool create ) :
-	Directory( Ceylan::encodeToROT13(directoryName) )
+
+
+EmbeddedDirectory::EmbeddedDirectory( const string & directoryName,
+  bool create ) :
+  Directory( Ceylan::encodeToROT13(directoryName) )
 {
 
 #if OSDL_USES_PHYSICSFS
 
-	EmbeddedFileSystemManager::SecureEmbeddedFileSystemManager() ;
-    
-	if ( create )
-    {
-    
-    	if ( PHYSFS_mkdir( directoryName.c_str() ) == 0 )
-        	throw DirectoryCreationFailed( 
-            	"EmbeddedDirectory constructor failed: "
-                + EmbeddedFileSystemManager::GetBackendLastError() ) ;
+  EmbeddedFileSystemManager::SecureEmbeddedFileSystemManager() ;
 
-    }
-     	
+  if ( create )
+  {
+
+	if ( PHYSFS_mkdir( directoryName.c_str() ) == 0 )
+	  throw DirectoryCreationFailed(
+		"EmbeddedDirectory constructor failed: "
+		+ EmbeddedFileSystemManager::GetBackendLastError() ) ;
+
+  }
+
 #else // OSDL_USES_PHYSICSFS
 
-	throw DirectoryException( "EmbeddedDirectory constructor failed: "
-    	"no PhysicsFS support available." ) ;  
+  throw DirectoryException( "EmbeddedDirectory constructor failed: "
+	"no PhysicsFS support available." ) ;
 
 #endif // OSDL_USES_PHYSICSFS
 
 }
 
-		
-		
-        	
-					
+
+
+
+
 // Protected section.
 
 
@@ -375,20 +375,19 @@ EmbeddedDirectory::EmbeddedDirectory( const string & directoryName,
 FileSystemManager & EmbeddedDirectory::getCorrespondingFileSystemManager() const
 {
 
-	try
-	{
-	
-		return EmbeddedFileSystemManager::GetEmbeddedFileSystemManager() ;
-	
-	} 
-	catch ( const EmbeddedFileSystemManagerException & e )
-	{
-	
-		throw DirectoryDelegatingException(
-			"EmbeddedDirectory::getCorrespondingFileSystemManager failed: "
-			+ e.toString() ) ;
-		
-	}	
-	
+  try
+  {
+
+	return EmbeddedFileSystemManager::GetEmbeddedFileSystemManager() ;
+
+  }
+  catch ( const EmbeddedFileSystemManagerException & e )
+  {
+
+	throw DirectoryDelegatingException(
+	  "EmbeddedDirectory::getCorrespondingFileSystemManager failed: "
+	  + e.toString() ) ;
+
+  }
+
 }
-																		
