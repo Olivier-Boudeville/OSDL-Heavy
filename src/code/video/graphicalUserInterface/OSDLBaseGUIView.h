c9197b81515c8cc703281e07b4bd95eaca425cb5
Olivier Boudeville <olivier.boudeville@esperide.com>
2010-07-01 14:58:13 +0000

    Reformatted.

--- src/code/video/graphicalUserInterface/OSDLBaseGUIView.h
+++ src/code/video/graphicalUserInterface/OSDLBaseGUIView.h
@@ -1,12 +1,12 @@
-/* 
- * Copyright (C) 2003-2009 Olivier Boudeville
+/*
+ * Copyright (C) 2003-2010 Olivier Boudeville
  *
  * This file is part of the OSDL library.
  *
  * The OSDL library is free software: you can redistribute it and/or modify
  * it under the terms of either the GNU Lesser General Public License or
  * the GNU General Public License, as they are published by the Free Software
- * Foundation, either version 3 of these Licenses, or (at your option) 
+ * Foundation, either version 3 of these Licenses, or (at your option)
  * any later version.
  *
  * The OSDL library is distributed in the hope that it will be useful,
@@ -45,106 +45,105 @@ namespace OSDL
 {
 
 
-		
-	namespace Rendering 
+
+	namespace Rendering
 	{
-	
-				
-				
+
+
+
 		/**
-		 * The base GUI view allows to set-up and perform the rendering
-		 * of a graphical user interface. 
+		 * The base GUI view allows to set-up and perform the rendering of a
+		 * graphical user interface.
 		 *
 		 * @see Renderer
 		 *
-		 */	
+		 */
 		class OSDL_DLL BaseGUIView : public Ceylan::MVC::BaseView
 		{
-		
-		
+
+
 			public:
-			
-			
-			
+
+
+
 				/**
 				 * Constructs a new base GUI view, used so that the graphical
 				 * user interface can be rendered.
 				 *
 				 */
 				explicit BaseGUIView() ;
-				
-				
-				
+
+
+
 				/**
 				 * Virtual destructor.
 				 *
 				 */
 				virtual ~BaseGUIView() throw() ;
-			
-			
+
+
 				/**
 				 * Requests that view to render the GUI.
 				 *
-				 * In this case no specific model is used, as a GUI is a 
-				 * pure model-less view.
+				 * In this case no specific model is used, as a GUI is a pure
+				 * model-less view.
 				 *
 				 */
 				virtual void render() ;
-			
-					
-	            /**
-	             * Returns an user-friendly description of the state of 
-				 * this object.
-	             *
+
+
+				/**
+				 * Returns an user-friendly description of the state of this
+				 * object.
+				 *
 				 * @param level the requested verbosity level.
 				 *
-				 * @note Text output format is determined from overall 
-				 * settings.
+				 * @note Text output format is determined from overall settings.
 				 *
 				 * @see Ceylan::TextDisplayable
-	             *
-	             */
-		 		virtual const std::string toString( 
+				 *
+				 */
+		 		virtual const std::string toString(
 					Ceylan::VerbosityLevels level = Ceylan::high ) const ;
 
-					
-					
-					
+
+
+
 			protected:
-			
-				 
-			
+
+
+
 			private:
-			
-			
-			
+
+
+
 				/**
-				 * Copy constructor made private to ensure that it will 
-				 * never be called.
+				 * Copy constructor made private to ensure that it will never be
+				 * called.
 				 *
-				 * The compiler should complain whenever this undefined 
+				 * The compiler should complain whenever this undefined
 				 * constructor is called, implicitly or not.
-				 * 
-				 */			 
-				explicit BaseGUIView( const BaseGUIView & source ) ;
-			
-			
-			
-				/**
-				 * Assignment operator made private to ensure that it 
-				 * will never be called.
 				 *
-				 * The compiler should complain whenever this undefined 
+				 */
+				explicit BaseGUIView( const BaseGUIView & source ) ;
+
+
+
+				/**
+				 * Assignment operator made private to ensure that it will never
+				 * be called.
+				 *
+				 * The compiler should complain whenever this undefined
 				 * operator is called, implicitly or not.
-				 * 
-				 */			 
+				 *
+				 */
 				BaseGUIView & operator = ( const BaseGUIView & source ) ;
-				
-				
-				
-				
+
+
+
+
 		} ;
-		
+
 
 	}
 
@@ -153,4 +152,3 @@ namespace OSDL
 
 
 #endif // OSDL_BASE_GUI_VIEW_H_
-
