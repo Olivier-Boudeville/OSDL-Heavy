81d293ef6f83500d8440b967c49232b8ca441358
Olivier Boudeville <olivier.boudeville@esperide.com>
2011-03-27 17:56:12 +0000

    Comments improved.

--- src/code/basic/OSDLHeaderVersion.h
+++ src/code/basic/OSDLHeaderVersion.h
@@ -39,12 +39,9 @@ namespace OSDL
 	 * This is the libtool version of the OSDL headers, as defined in the
 	 * configure step.
 	 *
-	 * Allows to detect run-time mismatches between the OSDL headers a
-	 * program or a library was compiled with, and the actual OSDL library
-	 * it is then linked to.
-	 *
-	 * This file is a non-UNIX (e.g. Windows) version: for UNIX, it is generated
-	 * from its *.h.in counterpart.
+	 * Allows to detect run-time mismatches between the OSDL headers a program
+	 * or a library was compiled with, and the actual OSDL library it is then
+	 * linked to.
 	 *
 	 */
 	OSDL_DLL const std::string actualOSDLHeaderLibtoolVersion
