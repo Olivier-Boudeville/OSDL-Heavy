93877ec58ad921dcca41040169d11cbb9748b72b
Olivier Boudeville <olivier.boudeville@esperide.com>
2011-03-27 18:05:54 +0000

    We had to fix again this Windows-only header.

--- src/code/basic/OSDLHeaderVersion.h
+++ src/code/basic/OSDLHeaderVersion.h
@@ -30,32 +30,41 @@
 #include <string>
 
 
+/*
+ * This file is dedicated to the Windows version, as on UNIX it is overwritten
+ * by the configure-time generated one.
+ *
+ * @note This Windows version needs OSDLHeaderVersion.cc, whereas the UNIX one
+ * does not.
+ *
+ */
+
+
+// Allowed because on Windows (hence not installed):
+#include "OSDLConfig.h"  // for  OSDL_LIBTOOL_VERSION
+
 
 namespace OSDL
 {
 
 
 	/**
-	 * This is the libtool version of the OSDL headers, as defined in the
-	 * configure step.
+	 * This is the Windows-compatible libtool version of the OSDL headers, as
+	 * defined in the configure step.
 	 *
 	 * Allows to detect run-time mismatches between the OSDL headers a program
 	 * or a library was compiled with, and the actual OSDL library it is then
 	 * linked to.
 	 *
-	 */
-	OSDL_DLL const std::string actualOSDLHeaderLibtoolVersion
-		= "0.5.0" ;
-
-
-	/*
-	 * NEVER check-in this file if there is no more OSDL_LIBTOOL_VERSION above!
+	 * @note Cannot declare here:
+	 * 'extern OSDL_DLL const std::string actualOSDLHeaderLibtoolVersion
+	 *	= OSDL_LIBTOOL_VERSION ;' because, with Visual C++, it leads to
+	 * multiple definitions for actualOSDLHeaderLibtoolVersion.
 	 *
 	 */
-
+	extern OSDL_DLL const std::string actualOSDLHeaderLibtoolVersion ;
 
 }
-
 
 
 #endif // OSDL_HEADER_VERSION_H_
