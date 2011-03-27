366789855ddab8a4a25bd49b143a559b92c22361
Olivier Boudeville <olivier.boudeville@esperide.com>
2011-03-27 18:07:52 +0000

    Reformatted.

--- src/code/engine/OSDLStandardRenderer.cc
+++ src/code/engine/OSDLStandardRenderer.cc
@@ -1,4 +1,4 @@
-/* 
+/*
  * Copyright (C) 2003-2011 Olivier Boudeville
  *
  * This file is part of the OSDL library.
@@ -6,7 +6,7 @@
  * The OSDL library is free software: you can redistribute it and/or modify
  * it under the terms of either the GNU Lesser General Public License or
  * the GNU General Public License, as they are published by the Free Software
- * Foundation, either version 3 of these Licenses, or (at your option) 
+ * Foundation, either version 3 of these Licenses, or (at your option)
  * any later version.
  *
  * The OSDL library is distributed in the hope that it will be useful,
@@ -38,7 +38,7 @@ using namespace OSDL::Engine ;
 
 
 #ifdef OSDL_USES_CONFIG_H
-#include <OSDLConfig.h>       // for OSDL_DEBUG_RENDERER and al 
+#include <OSDLConfig.h>       // for OSDL_DEBUG_RENDERER and al
 #endif // OSDL_USES_CONFIG_H
 
 
@@ -67,21 +67,21 @@ using std::list ;
 
 
 
-StandardRenderer::StandardRenderer( Video::Surface & screen, 
+StandardRenderer::StandardRenderer( Video::Surface & screen,
 		bool registerToScheduler ) :
 	Renderer( registerToScheduler ),
 	_screen( & screen )
 {
 
 	string message = "StandardRenderer created, " ;
-	
+
 	if ( ! registerToScheduler )
 		message += "not " ;
-	
+
 	message += "registred to scheduler." ;
-	
+
 	send( message ) ;
-	
+
 }
 
 
@@ -90,54 +90,54 @@ StandardRenderer::~StandardRenderer() throw()
 {
 
 	send( "Deleting StandardRenderer." ) ;
-	
+
 	// Views are not owned.
-	
+
 	Ceylan::Uint32 viewCount = _registeredViews.size() ;
-	
+
 	if ( viewCount != 0 )
 		LogPlug::warning( "StandardRenderer destructor: "
 			+ Ceylan::toString( viewCount ) + " view(s) still registered." ) ;
 
 	send( "StandardRenderer deleted." ) ;
-			
+
 }
 
 
 
 
-void StandardRenderer::registerView( Ceylan::MVC::BaseView & view ) 
+void StandardRenderer::registerView( Ceylan::MVC::BaseView & view )
 {
 
 	_registeredViews.push_back( & view ) ;
-	
+
 }
 
 
 
-void StandardRenderer::unregisterView( Ceylan::MVC::BaseView & view ) 
+void StandardRenderer::unregisterView( Ceylan::MVC::BaseView & view )
 {
 
 	list<Ceylan::MVC::BaseView *>::iterator it ;
-	
+
 	for ( it = _registeredViews.begin(); it != _registeredViews.end(); it++ )
 	{
-	
+
 		if ( (*it) == & view )
 		{
-		
+
 			_registeredViews.erase( it ) ;
-			
+
 			return ;
-		
+
 		}
-	
+
 	}
-	
+
 	if ( it == _registeredViews.end() )
 		throw RenderingException( "StandardRenderer::unregisterView: view '"
 			+ view.toString() + "' was not registered." ) ;
-						
+
 }
 
 
@@ -146,61 +146,60 @@ void StandardRenderer::render( RenderingTick currentRenderingTick )
 {
 
 	OSDL_RENDER_LOG( "Standard rendering!" ) ;
-	
+
 	_screen->clear() ;
-	
-			
+
+
 	for ( list<Ceylan::MVC::BaseView *>::iterator it = _registeredViews.begin();
 			it != _registeredViews.end(); it++ )
 		(*it)->render() ;
-		
+
 	_screen->update() ;
-	
+
 	/*
-	 * Beware, currentRenderingTick might be always zero if no scheduler 
-	 * is used.
+	 * Beware, currentRenderingTick might be always zero if no scheduler is
+	 * used.
 	 *
 	 */
 	_renderingDone++ ;
-	
+
 }
 
 
 
-void StandardRenderer::onRenderingSkipped( 
+void StandardRenderer::onRenderingSkipped(
 	Events::RenderingTick skippedRenderingTick )
 {
 
-	OSDL_RENDER_LOG( "Rendering tick " 
+	OSDL_RENDER_LOG( "Rendering tick "
 		+ Ceylan::toString( skippedRenderingTick ) + " had to be skipped." ) ;
 
 	/*
 	 TO-DO: add an OSDL base view, inheriting from Ceylan's one
 	 *and* from OSDL::Engine::ScheduledView which supports an onSkip/1 method
-	 
+
 	for ( list<Ceylan::MVC::BaseView *>::iterator it = _registeredViews.begin();
 			it != _registeredViews.end(); it++ )
 		(*it)->onSkip( skippedRenderingTick ) ;
 	 */
 }
-	
+
 
 
 const string StandardRenderer::toString( Ceylan::VerbosityLevels level ) const
 {
 
-	string res = "Standard renderer, last rendering tick was " 
+	string res = "Standard renderer, last rendering tick was "
 		+ Ceylan::toString( _lastRender )
-		+ ", having performed " 
+		+ ", having performed "
 		+ Ceylan::toString( _renderingDone )    + " rendering(s) for "
 		+ Ceylan::toString( _renderingSkipped ) + " skip(s)" ;
-	
-	if ( _renderingDone + _renderingSkipped != 0 )
-		res += " (rendering proportion: " 
-			+ Ceylan::toString( 100 * _renderingDone / ( _renderingDone 
-				+ _renderingSkipped ) )	+ "%)" ;
-			
-	return res ;
-	
-}
 
+	if ( _renderingDone + _renderingSkipped != 0 )
+		res += " (rendering proportion: "
+			+ Ceylan::toString( 100 * _renderingDone / ( _renderingDone
+				+ _renderingSkipped ) ) + "%)" ;
+
+	return res ;
+
+}
