d660311ee5b3c5f7395491dc853298fc06f5e546
Olivier Boudeville <olivier.boudeville@esperide.com>
2011-06-17 19:25:24 +0000

    Includes fixed.

--- src/code/video/graphicalUserInterface/OSDLGraphicalUserInterfaceIncludes.h
+++ src/code/video/graphicalUserInterface/OSDLGraphicalUserInterfaceIncludes.h
@@ -1,4 +1,4 @@
-/* 
+/*
  * Copyright (C) 2003-2011 Olivier Boudeville
  *
  * This file is part of the OSDL library.
@@ -6,7 +6,7 @@
  * The OSDL library is free software: you can redistribute it and/or modify
  * it under the terms of either the GNU Lesser General Public License or
  * the GNU General Public License, as they are published by the Free Software
- * Foundation, either version 3 of these Licenses, or (at your option) 
+ * Foundation, either version 3 of these Licenses, or (at your option)
  * any later version.
  *
  * The OSDL library is distributed in the hope that it will be useful,
@@ -29,14 +29,12 @@
 
 
 /*
- * This include repository keeps track of headers of the module dedicated
- * to graphical user interface rendering.
+ * This include repository keeps track of headers of the module dedicated to
+ * graphical user interface rendering.
  *
  */
 
 #include "OSDLBaseGUIView.h"
-#include "OSDLGUICommon.h"
 
- 
+
 #endif // OSDL_GRAPHICAL_USER_INTERFACE_INCLUDES_H_
-
